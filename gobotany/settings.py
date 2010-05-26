94504f104341f3d4c79949fea0dcd9951b579983
Rocky Burt <rocky@serverzen.com>
2010-05-26 21:10:58 +0000

    Updated settings to work with local buildout based postgresql
    git-svn-id: http://svn.jazkarta.com/newfs-database-api/gobotany/trunk/src@7 fafe9cc9-101d-4c6e-bffa-87022359e7d1

---
+++ gobotany/settings.py
@@ -0,0 +1,7 @@
+DATABASE_ENGINE = 'postgresql_psycopg2'
+DATABASE_NAME = 'gobotany'
+DATABASE_HOST = '/tmp'
+DATABASE_USER = ''
+DATABASE_PASSWORD = ''
+INSTALLED_APPS = ['gobotany']
+ROOT_URLCONF = ['gobotany.urls']
--- gobotany/testsettings.py
+++ gobotany/testsettings.py
@@ -1,4 +1,5 @@
+from gobotany.settings import (ROOT_URLCONF,
+                               INSTALLED_APPS)
+
 DATABASE_ENGINE = 'sqlite3'
 DATABASE_NAME = '/tmp/gobotany.db'
-INSTALLED_APPS = ['gobotany']
-ROOT_URLCONF = ['gobotany.urls']
