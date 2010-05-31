58dfd53fbab567e2f3d94acdb183436bcfa3678a
Rocky Burt <rocky@serverzen.com>
2010-05-31 21:50:45 +0000

    Added first rest support
    git-svn-id: http://svn.jazkarta.com/newfs-database-api/gobotany/trunk/src@21 fafe9cc9-101d-4c6e-bffa-87022359e7d1

---
+++ gobotany/handlers.py
@@ -0,0 +1,44 @@
+from gobotany import botany
+from piston.handler import BaseHandler
+
+
+def serialize(obj):
+    return obj.__dict__
+
+
+class TaxonQueryHandler(BaseHandler):
+    methods_allowed = ('GET',)
+
+    def read(self, request):
+        kwargs = {}
+        for k, v in request.GET.items():
+            kwargs[k] = v
+        species = botany.query_species(**kwargs)
+        listing = []
+        for s in species:
+            res = {}
+            s = species[0]
+            res['scientific_name'] = s.scientific_name
+            res['id'] = s.id
+            res['pile'] = s.pile.name
+            for cv in s.character_values.all():
+                res[cv.character.short_name] = cv.value
+            listing.append(res)
+        return listing
+
+
+class TaxonHandler(BaseHandler):
+    methods_allowed = ('GET',)
+
+    def read(self, request, scientific_name=None):
+        species = botany.query_species(scientific_name=scientific_name)
+        if len(species) > 0:
+            res = {}
+            s = species[0]
+            res['scientific_name'] = s.scientific_name
+            res['id'] = s.id
+            res['pile'] = s.pile.name
+            for cv in s.character_values.all():
+                res[cv.character.short_name] = cv.value
+            return res
+        return {}
--- gobotany/urls.py
+++ gobotany/urls.py
@@ -1,4 +1,5 @@
-from gobotany import views
+from gobotany import views, handlers
+from piston.resource import Resource
 from django.conf.urls.defaults import *
 from django.contrib import admin
 admin.autodiscover()
@@ -6,5 +7,11 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
     ('^$', views.default_view),
+
+    (r'^taxon/(?P<scientific_name>[^/]+)/$',
+     Resource(handler=handlers.TaxonHandler)),
+    (r'^taxon/$',
+     Resource(handler=handlers.TaxonQueryHandler)),
+
     (r'^admin/', include(admin.site.urls)),
 )
