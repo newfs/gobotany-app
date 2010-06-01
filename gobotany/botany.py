da2deadc7acbfb6fc51345591575a6001666e43c
Alec Mitchell <alecpm@gmail.com>
2010-06-01 20:58:48 +0000

    Optimize!
    git-svn-id: http://svn.jazkarta.com/newfs-database-api/gobotany/trunk/src@24 fafe9cc9-101d-4c6e-bffa-87022359e7d1

--- gobotany/botany.py
+++ gobotany/botany.py
@@ -7,24 +7,15 @@ class SpeciesReader(object):
                       scientific_name=None,
                       **kwargs):
 
-        res = []
         if scientific_name:
-            for x in models.Taxon.objects.filter(
-                scientific_name=scientific_name):
-                res.append(x)
+            return models.Taxon.objects.filter(scientific_name=scientific_name)
         else:
+            base_query = models.Taxon.objects
             for k, v in kwargs.items():
-                chars = models.Character.objects.filter(short_name=k)
-                for char in chars:
-                    cvs = models.CharacterValue.objects.filter(value=v,
-                                                               character=char)
-                    for cv in cvs:
-                        tcvs = models.TaxonToCharacterValue.objects.filter(
-                            character_value=cv)
-                        for tcv in tcvs:
-                            res.append(tcv.taxon)
-
-        return res
+                 base_query = base_query.filter(
+                     character_values__character__short_name=k,
+                     character_values__value=v)
+            return base_query
 
 _reader = SpeciesReader()
 query_species = _reader.query_species
