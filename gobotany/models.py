ce7af2896c74fbd9f6e9c9bcc10fc9b515a9ff94
Alec Mitchell <alecpm@gmail.com>
2010-06-02 18:14:40 +0000

    Add glossary models and update constraints on existing models.
    git-svn-id: http://svn.jazkarta.com/newfs-database-api/gobotany/trunk/src@32 fafe9cc9-101d-4c6e-bffa-87022359e7d1

--- gobotany/models.py
+++ gobotany/models.py
@@ -2,16 +2,29 @@ from django.db import models
 
 
 class CharacterGroup(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     def __unicode__(self):
         return u'CharacterGroup: %s id=%s' % (self.name, self.id)
 
 
+class GlossaryTerm(models.Model):
+    term = models.CharField(max_length=100)
+    lay_definition = models.TextField()
+    formal_definition = models.TextField(blank=True)
+    hint = models.TextField(blank=True)
+    visible = models.BooleanField(default=True)
+    # XXX: images
+
+
 class Character(models.Model):
-    short_name = models.CharField(max_length=100)
+    short_name = models.CharField(max_length=100, unique=True)
     name = models.CharField(max_length=100)
     character_group = models.ForeignKey(CharacterGroup)
+    glossary_terms = models.ManyToManyField(GlossaryTerm,
+                                         through='GlossaryTermForPileCharacter',
+                                         blank=True,
+                                         null=True)
 
     def __unicode__(self):
         return u'Character: %s name=%s id=%s' % (self.short_name, self.name,
@@ -21,6 +34,7 @@ class Character(models.Model):
 class CharacterValue(models.Model):
     value = models.CharField(max_length=100)
     character = models.ForeignKey(Character)
+    glossary_term = models.ForeignKey(GlossaryTerm, blank=True, null=True)
 
     def __unicode__(self):
         return u'CharacterValue: character=%s value=%s id=%s' % (
@@ -30,7 +44,7 @@ class CharacterValue(models.Model):
 
 
 class Pile(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     description = models.CharField(max_length=250)
     character_values = models.ManyToManyField(CharacterValue)
 
@@ -38,10 +52,17 @@ class Pile(models.Model):
         return u'Pile: %s id=%s' % (self.name, self.id)
 
 
+class GlossaryTermForPileCharacter(models.Model):
+    character = models.ForeignKey(Character)
+    pile = models.ForeignKey(Pile)
+    glossary_term = models.ForeignKey(GlossaryTerm)
+
+
 class Taxon(models.Model):
-    scientific_name = models.CharField(max_length=100)
+    scientific_name = models.CharField(max_length=100, unique=True)
     character_values = models.ManyToManyField(CharacterValue,
                                               through='TaxonToCharacterValue')
+    taxonomic_authority = models.CharField(max_length=100)
     pile = models.ForeignKey(Pile)
 
     def __unicode__(self):
