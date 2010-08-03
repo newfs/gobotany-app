c638667b84c71a05de9dc082dcaaa146a968b36a
Brandon Rhodes <brandon@rhodesmill.org>
2010-08-03 15:28:42 +0000

    Added some sanity-check tests for the initial collections page, and started writing a test for the subway map.
    git-svn-id: http://svn.jazkarta.com/newfs-database-api/gobotany/trunk/src@148 fafe9cc9-101d-4c6e-bffa-87022359e7d1

--- gobotany/simplekey/tests.py
+++ gobotany/simplekey/tests.py
@@ -7,4 +7,16 @@ class SimpleTests(TestCase):
 
     def test_collection_start_page(self):
         c = Client()
-        c.get('/simple/collections/start/')
+        r = c.get('/simple/collections/start/')
+
+        assert 'Ferns' in r.content
+        assert 'Woody plants' in r.content
+        assert 'Give me more choices' in r.content
+
+        assert '/simple/collections/aquatics/' in r.content
+        assert '/simple/collections/continue1/' in r.content
+
+    def test_subway_map(self):
+        c = Client()
+        r = c.get('/simple/collections/start/')
+        # put complicated test here
