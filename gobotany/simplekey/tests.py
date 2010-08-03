ffe5925aa638eee4f3bf3e4b108fbc441d2adc82
Brandon Rhodes <brandon@rhodesmill.org>
2010-08-03 01:23:57 +0000

    Added a simple test of whether the initial "Collections" page loads.
    git-svn-id: http://svn.jazkarta.com/newfs-database-api/gobotany/trunk/src@136 fafe9cc9-101d-4c6e-bffa-87022359e7d1

--- gobotany/simplekey/tests.py
+++ gobotany/simplekey/tests.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
 from django.test.client import Client
 
+from gobotany.simplekey.models import Collection
+
 class SimpleTests(TestCase):
 
-    def test_environment(self):
-        self.assert_(True)
+    def test_collection_start_page(self):
+        c = Client()
+        c.get('/simple/collections/start/')
