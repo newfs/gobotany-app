bf9edf6b90ffcab49a97d9acfe73afcfe2265ff6
Brandon Rhodes <brandon@rhodesmill.org>
2010-08-03 00:20:06 +0000

    Created the super-pile pages specified in the pile-groups PDF.
    git-svn-id: http://svn.jazkarta.com/newfs-database-api/gobotany/trunk/src@134 fafe9cc9-101d-4c6e-bffa-87022359e7d1

--- gobotany/simplekey/models.py
+++ gobotany/simplekey/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class Collection(models.Model):
     slug = models.CharField(max_length=20, unique=True)
     title = models.CharField(max_length=50)
-    blurb = models.TextField(blank=True)
     contents = models.TextField(blank=True)
 
     class Meta:
