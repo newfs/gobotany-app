9fe9b55b01ad1c38a64fcd1f040770d1efb1991a
John Galada <jg344@fastmail.net>
2010-08-04 19:28:24 +0000

    Commit change to JS code for the filter list to use Rocky's suggestion for getting the individual pile resource. Change to use new slug in pile REST URI.
    git-svn-id: http://svn.jazkarta.com/newfs-database-api/gobotany/trunk/src@162 fafe9cc9-101d-4c6e-bffa-87022359e7d1

--- gobotany/static/gobotany/results.js
+++ gobotany/static/gobotany/results.js
@@ -1,27 +1,24 @@
 dojo.provide('gobotany.results');
 dojo.require('dojox.data.JsonRestStore');
 
-gobotany.results.populate_default_filters = function(pile) {
+gobotany.results.populate_default_filters = function(pile_slug) {
     var filtersList = dojo.query('#filters ul')[0];
     dojo.empty(filtersList);
 
-    var url = '/piles/'; // + pile; // ? for some reason fetch isn't returning
-                                    // when pile added
+    var url = '/piles/';
     var store = new dojox.data.JsonRestStore({target: url});
-    store.fetch({onComplete: function(response) {
-        for (var x = 0; x < response.items.length; x++) {
-            var item = response.items[x];
-            if (item.name === pile) {
-                for (var y = 0; y < item.default_filters.length; y++) {
-                    var filter = item.default_filters[y];
-                    dojo.place('<li>' + filter.character_friendly_name + 
-                               '</li>', filtersList);
-                }
+    store.fetchItemByIdentity({
+        identity: pile_slug,
+        onItem: function(item) {
+            for (var y = 0; y < item.default_filters.length; y++) {
+                var filter = item.default_filters[y];
+                dojo.place('<li>' + filter.character_friendly_name +
+                           '</li>', filtersList);
             }
         }
-    }});
+    });
 };
 
-gobotany.results.init = function(pile) {
-    gobotany.results.populate_default_filters(pile);
+gobotany.results.init = function(pile_slug) {
+    gobotany.results.populate_default_filters(pile_slug);
 };
