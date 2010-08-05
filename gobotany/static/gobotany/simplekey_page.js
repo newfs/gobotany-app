58eaea764ad19256c27edb53a80ef8897d78269a
Alec Mitchell <alecpm@gmail.com>
2010-08-05 13:45:40 +0000

    Finish implementation of User Story 3 tooltips.  Use new gobotany base template for pile selection pages.
    git-svn-id: http://svn.jazkarta.com/newfs-database-api/gobotany/trunk/src@168 fafe9cc9-101d-4c6e-bffa-87022359e7d1

---
+++ gobotany/static/gobotany/simplekey_page.js
@@ -0,0 +1,13 @@
+dojo.provide('gobotany.simplekey_page');
+dojo.require("dijit.Dialog");
+dojo.require("dijit.form.Button");
+dojo.require('dojox.embed.Flash');
+dojo.addOnLoad(function() {
+    dojo.query('.PileVideo').forEach(function (node, index, attr) {
+        var path = dojo.attr(dojo.query('> a', node)[0], 'href');
+        var player = new dojox.embed.Flash({path: path,
+                                            width: 200,
+                                            height: 200},
+                                            node);
+    });
+});
