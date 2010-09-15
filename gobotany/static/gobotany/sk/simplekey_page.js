6a91ea5c1899ac275a2dff4682eccc694c5cc47f
Brandon Rhodes <brandon@rhodesmill.org>
2010-09-15 15:03:51 +0000

    Made the images in the image-ribbon clickable, bringing up a tooltip box.
    git-svn-id: http://svn.jazkarta.com/newfs-database-api/gobotany/trunk/src@421 fafe9cc9-101d-4c6e-bffa-87022359e7d1

--- gobotany/static/gobotany/sk/simplekey_page.js
+++ gobotany/static/gobotany/sk/simplekey_page.js
@@ -1,7 +1,9 @@
 dojo.provide('gobotany.sk.simplekey_page');
+dojo.require('gobotany.sk.plant_preview');
 dojo.require("dijit.Dialog");
-dojo.require("dijit.Tooltip");
+dojo.require("dijit.TooltipDialog");
 dojo.require("dijit.form.Button");
+dojo.require("dijit.form.DropDownButton");
 dojo.require('dojox.embed.Flash');
 dojo.addOnLoad(function() {
     dojo.query('.PileVideo').forEach(function (node, index, attr) {
