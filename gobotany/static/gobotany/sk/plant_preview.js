befc694703f83464bd44c4a3f8014ced102c1167
John Galada <jg344@fastmail.net>
2010-10-09 14:23:25 +0000

    Add the ability to browse images to the species page. Extract common code shared between the species page and the plant preview popups on the results page.
    git-svn-id: http://svn.jazkarta.com/newfs-database-api/gobotany/trunk/src@540 fafe9cc9-101d-4c6e-bffa-87022359e7d1

--- gobotany/static/gobotany/sk/plant_preview.js
+++ gobotany/static/gobotany/sk/plant_preview.js
@@ -1,63 +1,33 @@
 dojo.provide('gobotany.sk.plant_preview');
 
 dojo.require('dojox.data.JsonRestStore');
+dojo.require('gobotany.sk.image_browse');
 
 // Image info storage for images that appear on the plant preview dialog box.
-gobotany.sk.plant_preview.plant_preview_images = [];
+gobotany.sk.plant_preview.images = [];
 
-gobotany.sk.plant_preview.change_plant_preview_image = function(event) {
+gobotany.sk.plant_preview.change_image = function(event) {
     event.preventDefault();
-    //alert('this.innerHTML: ' + this.innerHTML);
+
     var img = dojo.query('#plant-preview .photos img')[0];
     var current_image_url = dojo.attr(img, 'src');
 
-    var current_image_index = null;
-    var i = 0;
-    while (current_image_index === null &&
-           i < gobotany.sk.plant_preview.plant_preview_images.length) {
-        if (current_image_url ===
-            gobotany.sk.plant_preview.plant_preview_images[i].scaled_url) {
-
-            current_image_index = i;
-        }
-        i++;
-    }
+    // Figure out the index of the image that is showing.
+    var current_image_index =
+        gobotany.sk.image_browse.get_current_image_index(current_image_url,
+            gobotany.sk.plant_preview.images, 'scaled_url');
 
     // Figure out which image to show next.
-    var new_image_index = current_image_index;
-    if (this.innerHTML.indexOf('next') >= 0) {
-        new_image_index++;
-    }
-    else if (this.innerHTML.indexOf('prev') >= 0) {
-        new_image_index--;
-    }
-    if (new_image_index < 0) {
-        new_image_index =
-            gobotany.sk.plant_preview.plant_preview_images.length - 1;
-    }
-    else if (new_image_index >
-             gobotany.sk.plant_preview.plant_preview_images.length - 1) {
-        new_image_index = 0;
-    }
+    var new_image_index = gobotany.sk.image_browse.get_next_image_index(
+        '#plant-preview .photos', gobotany.sk.plant_preview.images.length,
+        current_image_index);
 
-    // Change the image.
-    dojo.attr(img, 'src', gobotany.sk.plant_preview.plant_preview_images[
-                          new_image_index].scaled_url);
-    dojo.attr(img, 'alt', gobotany.sk.plant_preview.plant_preview_images[
-                          new_image_index].title);
-    // Update the position/count message.
+    // Change the image and update the position/count message.
     var msg = dojo.query('#plant-preview .photos span')[0];
-    msg.innerHTML = (new_image_index + 1) + ' of ' +
-        gobotany.sk.plant_preview.plant_preview_images.length;
-};
-
-gobotany.sk.plant_preview.set_image = function(image_node, message_node,
-                                      plant_image, index, num_images) {
-    dojo.attr(image_node, 'src', plant_image.scaled_url);
-    // TODO: is this title field intended/sufficient for alt text?
-    dojo.attr(image_node, 'alt', plant_image.title);
-    // Set the count message.
-    message_node.innerHTML = (index + 1) + ' of ' + num_images;
+    var image = gobotany.sk.plant_preview.images[new_image_index];
+    gobotany.sk.image_browse.set_image(img, msg, image.scaled_url,
+        image.title, new_image_index, 
+        gobotany.sk.plant_preview.images.length);
 };
 
 gobotany.sk.plant_preview.show_plant_preview = function(plant,
@@ -107,48 +77,38 @@ gobotany.sk.plant_preview.show_plant_preview = function(plant,
             }
             dojo.create('dd', {innerHTML: piles}, list);
 
-            gobotany.sk.plant_preview.plant_preview_images = [];
+            gobotany.sk.plant_preview.images = [];
             if (taxon.images.length) {
                 // Store the info for the images to allow for browsing them.
                 for (var i = 0; i < taxon.images.length; i++) {
-                    gobotany.sk.plant_preview.plant_preview_images.push(
-                        taxon.images[i]);
+                    gobotany.sk.plant_preview.images.push(taxon.images[i]);
                 }
                 // If the alt text of the thumbnail the user clicked on in the
                 // page is different from the alt text of the first image
                 // showing on the popup, look for matching alt text and show
-                // that image first on the popup. These will
+                // that image first on the popup.
                 var clicked_image_alt_text = dojo.attr(clicked_image, 'alt');
                 var first_image_index = 0;
-                for (var i = 0;
-                    i < gobotany.sk.plant_preview.plant_preview_images.length;
+                for (var i = 0; i < gobotany.sk.plant_preview.images.length;
                     i++) {
 
                     if (clicked_image_alt_text ===
-                        gobotany.sk.plant_preview.plant_preview_images[i].title) {
+                        gobotany.sk.plant_preview.images[i].title) {
 
                         first_image_index = i;
                         break;
                     }
                 }
-                gobotany.sk.plant_preview.set_image(img, msg,
-                    gobotany.sk.plant_preview.plant_preview_images[first_image_index],
-                    first_image_index, taxon.images.length);
+                var image = 
+                    gobotany.sk.plant_preview.images[first_image_index];
+                gobotany.sk.image_browse.set_image(img, msg, image.scaled_url,
+                    image.title, first_image_index, taxon.images.length);
             }
 
             // Wire up the previous and next links, or hide them if
             // they're not needed.
-            var links = dojo.query('#plant-preview .photos a');
-            for (var i = 0; i < links.length; i++) {
-                if (taxon.images.length > 1) {
-                    dojo.removeClass(links[i], 'hidden');
-                    dojo.connect(links[i], 'onclick',
-                        gobotany.sk.plant_preview.change_plant_preview_image);
-                }
-                else {
-                    dojo.addClass(links[i], 'hidden');
-                }
-            }
+            gobotany.sk.image_browse.set_up_navigation_links(taxon.images,
+                '#plant-preview .photos', gobotany.sk.plant_preview.change_image);
         }
     });
 };
--- gobotany/static/gobotany/sk/species.js
+++ gobotany/static/gobotany/sk/species.js
@@ -1,1 +1,78 @@
 dojo.provide('gobotany.sk.species');
+
+dojo.require('dojox.data.JsonRestStore');
+dojo.require('gobotany.sk.image_browse');
+
+// Image info storage for images that appear on the species page.
+gobotany.sk.species.images = [];
+
+gobotany.sk.species.change_image = function(event) {
+    event.preventDefault();
+
+    var img = dojo.query('#species #images img')[0];
+    var current_image_url = dojo.attr(img, 'src');
+
+    // Figure out the index of the image that is showing.
+    var current_image_index =
+        gobotany.sk.image_browse.get_current_image_index(current_image_url,
+            gobotany.sk.species.images, 'url');
+
+    // Figure out which image to show next.
+    var new_image_index = gobotany.sk.image_browse.get_next_image_index(
+        '#species #images', gobotany.sk.species.images.length,
+        current_image_index);
+
+    // Change the image and update the position/count message.
+    var msg = dojo.query('#species #images span')[0];
+    var image = gobotany.sk.species.images[new_image_index];
+    gobotany.sk.image_browse.set_image(img, msg, image.url,
+        image.title, new_image_index, gobotany.sk.species.images.length);
+};
+
+gobotany.sk.species.init = function(scientific_name) {
+    var img = dojo.query('#species #images img')[0];
+    var msg = dojo.query('#species #images span')[0];
+    
+    // Load the taxon URL and set up the images.
+    taxon_url = '/taxon/' + scientific_name + '/';
+    var taxon_store = new dojox.data.JsonRestStore({target: taxon_url});
+    taxon_store.fetch({
+        onComplete: function(taxon) {
+            gobotany.sk.species.images = [];
+            if (taxon.images.length) {
+                // Store the info for the images to allow for browsing them.
+                for (var i = 0; i < taxon.images.length; i++) {
+                    gobotany.sk.species.images.push(taxon.images[i]);
+                }
+                // If the alt text of the thumbnail the user clicked on in the
+                // page is different from the alt text of the first image
+                // showing on the popup, look for matching alt text and show
+                // that image first on the popup.
+                
+                // TODO: pass image that was visible on the plant preview
+                // popup when the user came to the species page.
+                //var clicked_image_alt_text = dojo.attr(clicked_image, 'alt');
+                var preview_image_alt_text = 'TODO';
+                
+                var first_image_index = 0;
+                for (i = 0; i < gobotany.sk.species.images.length; i++) {
+
+                    if (preview_image_alt_text ===
+                        gobotany.sk.species.images[i].title) {
+
+                        first_image_index = i;
+                        break;
+                    }
+                }
+                var image = gobotany.sk.species.images[first_image_index];
+                gobotany.sk.image_browse.set_image(img, msg, image.url,
+                    image.title, first_image_index, taxon.images.length);
+            }
+
+            // Wire up the previous and next links, or hide them if
+            // they're not needed.
+            gobotany.sk.image_browse.set_up_navigation_links(taxon.images,
+                '#species #images', gobotany.sk.species.change_image);
+        }
+    });
+};
