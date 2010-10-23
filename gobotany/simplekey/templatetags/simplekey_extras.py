d9c609c0b98985fb11f22298c7d86c17dea1c6c7
John Galada <jg344@fastmail.net>
2010-10-23 11:50:13 +0000

    Tweak template filters and comments.
    git-svn-id: http://svn.jazkarta.com/newfs-database-api/gobotany/trunk/src@574 fafe9cc9-101d-4c6e-bffa-87022359e7d1

--- gobotany/simplekey/templatetags/simplekey_extras.py
+++ gobotany/simplekey/templatetags/simplekey_extras.py
@@ -5,14 +5,14 @@ register = template.Library()
 
 @stringfilter
 def split(value, arg):
-    '''Splits a string into a list on the given character.'''
+    '''Split a string into a list on the given character.'''
     return value.split(arg)
 
 
-def index(value, arg):
+def at_index(value, arg):
     '''Return the list item at the specified index.'''
     return value[int(arg)]
 
 
 register.filter('split', split)
-register.filter('index', index)
+register.filter('at_index', at_index)
