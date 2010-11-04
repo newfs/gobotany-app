2faf537ebc53d8042f6718b75416e8f3343be175
Brandon Rhodes <brandon@rhodesmill.org>
2010-11-04 19:23:31 +0000

    Split the ruler-slider into a separate file for easier testing.
    git-svn-id: http://svn.jazkarta.com/newfs-database-api/gobotany/trunk/src@609 fafe9cc9-101d-4c6e-bffa-87022359e7d1

---
+++ gobotany/static/gobotany/sk/RulerSlider.js
@@ -0,0 +1,75 @@
+/* Length slider that looks something like a ruler. */
+
+dojo.provide('gobotany.sk.RulerSlider');
+dojo.require('dijit.form.HorizontalSlider');
+dojo.require('dijit.form.HorizontalRuleLabels');
+
+dojo.declare('gobotany.sk.RulerSlider', null, {
+
+    constructor: function(node, themin, themax, startvalue, updater) {
+
+        var slider = new dijit.form.HorizontalSlider({
+            name: 'character_slider',
+            showButtons: false,
+            value: startvalue,
+            minimum: themin,
+            maximum: themax,
+            intermediateChanges: true,
+            style: 'width: 600px;',
+            onChange: dojo.hitch(updater, updater.update_fields)
+        }, node);
+
+        var ticks_count = this.get_ticks_labels_count(themin, themax);
+
+        var rule_node = dojo.create('div', null, slider.containerNode);
+        var ruleticks = new dijit.form.HorizontalRule({
+            container: 'topDecoration',
+            count: ticks_count,
+            style: 'height: 10px;'
+        }, rule_node);
+
+        var labels_node = dojo.create('div', null, slider.containerNode);
+        var mylabels = this.get_labels(themin, themax, ticks_count);
+        var rule_labels = new dijit.form.HorizontalRuleLabels({
+            container: 'topDecoration',
+            count: ticks_count,
+            labels: mylabels,
+            style: 'height:1.5em; font-size:75%; color:#000; width: 600px;'
+        }, labels_node);
+
+        var rule_node2 = dojo.create('div', null, slider.containerNode);
+        var ruleticks2 = new dijit.form.HorizontalRule({
+            container: 'topDecoration',
+            count: ticks_count,
+            style: 'height: 10px;'
+        }, rule_node2);
+    },
+
+    get_ticks_labels_count: function(min, max) {
+        // Get the number of ticks and labels for the slider.
+        // If the number is too high for the allotted screen space,
+        // cut it down to a reasonable number.
+        var MAX_NUMBER = 10;
+        var number = max - min + 1;
+        while (number > MAX_NUMBER) {
+            number = Math.round(number / 2);
+        }
+        return number;
+    },
+
+    get_labels: function(min, max, number_of_ticks) {
+        // Return a list of labels for the given range and number of ticks.
+        var labels = [];
+        var number_of_segments = number_of_ticks - 1;
+
+        labels.push('');
+        for (i = 1; i < number_of_segments; i++) {
+            var label_value = ((max - min) / number_of_segments) * i;
+            labels.push(String(label_value.toFixed(1)));
+        }
+        labels.push(String(max.toFixed(1)) + '<br>mm');
+
+        return labels;
+    }
+
+})
--- gobotany/static/gobotany/sk/results.js
+++ gobotany/static/gobotany/sk/results.js
@@ -5,6 +5,7 @@
 
 dojo.provide('gobotany.sk.results');
 
+dojo.require('gobotany.sk.RulerSlider');
 dojo.require('gobotany.sk.glossarize');
 dojo.require('gobotany.sk.results.SpeciesSectionHelper');
 dojo.require('gobotany.filters');
@@ -20,8 +21,6 @@ dojo.require('dijit.form.Button');
 dojo.require('dijit.form.CheckBox');
 dojo.require("dijit.form.FilteringSelect");
 dojo.require('dijit.form.Form');
-dojo.require('dijit.form.HorizontalSlider');
-dojo.require("dijit.form.HorizontalRuleLabels");
 dojo.require('dijit.form.Select');
 
 dojo.declare('gobotany.sk.results.ResultsHelper', null, {
@@ -678,33 +677,6 @@ dojo.declare('gobotany.sk.results.FilterSectionHelper', null, {
         return 0; // default value (no sort)
     },
 
-    get_ticks_labels_count: function(min, max) {
-        // Get the number of ticks and labels for the slider.
-        // If the number is too high for the allotted screen space,
-        // cut it down to a reasonable number.
-        var MAX_NUMBER = 10;
-        var number = max - min + 1;
-        while (number > MAX_NUMBER) {
-            number = Math.round(number / 2);
-        }
-        return number;
-    },
-
-    get_labels: function(min, max, number_of_ticks) {
-        // Return a list of labels for the given range and number of ticks.
-        var labels = [];
-        var number_of_segments = number_of_ticks - 1;
-
-        labels.push('');
-        for (i = 1; i < number_of_segments; i++) {
-            var label_value = ((max - min) / number_of_segments) * i;
-            labels.push(String(label_value.toFixed(1)));
-        }
-        labels.push(String(max.toFixed(1)));
-
-        return labels;
-    },
-
     show_filter_working: function(filter) {
         dojo.query('#filter-working').style({display: 'block'});
         dojo.query('#filter-working .name')[0].innerHTML = filter.friendly_name;
@@ -773,35 +745,8 @@ dojo.declare('gobotany.sk.results.FilterSectionHelper', null, {
             updater.update_fields(startvalue);
 
             var slider_node = dojo.create('div', null, valuesList);
-            var slider = new dijit.form.HorizontalSlider({
-                name: 'character_slider',
-                showButtons: false,
-                value: startvalue,
-                minimum: themin,
-                maximum: themax,
-                intermediateChanges: true,
-                style: 'width:200px;',
-                onChange: dojo.hitch(updater, updater.update_fields)
-            }, slider_node);
-
-            var ticks_count = this.get_ticks_labels_count(themin, themax);
-
-            var rule_node = dojo.create('div', null, slider.containerNode);
-            var ruleticks = new dijit.form.HorizontalRule({
-                container: 'topDecoration',
-                count: ticks_count,
-                style: 'height:10px;'
-            }, rule_node);
-
-            var labels_node = dojo.create('div', null, slider.containerNode);
-            var mylabels = this.get_labels(themin, themax, ticks_count);
-
-            var rule_labels = new dijit.form.HorizontalRuleLabels({
-                container: 'bottomDecoration',
-                count: ticks_count,
-                labels: mylabels,
-                style: 'height:1.5em; font-size:75%; color:#000; width:200px;'
-            }, labels_node);
+            new gobotany.sk.RulerSlider(slider_node, themin, themax,
+                                        startvalue, updater);
 
         } else {
 
