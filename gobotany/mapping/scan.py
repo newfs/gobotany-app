8a7e829c9443609f59d3ee8269b07eaacd001334
Brandon Rhodes <brandon@rhodesmill.org>
2011-08-17 16:47:37 +0000

    Converted the map scanner so that it outputs a CSV file with its findings.
    git-svn-id: http://svn.jazkarta.com/newfs-database-api/gobotany/trunk/src@1395 fafe9cc9-101d-4c6e-bffa-87022359e7d1

--- gobotany/mapping/scan.py
+++ gobotany/mapping/scan.py
@@ -1,8 +1,12 @@
 """Scan a map to learn how a species is distributed."""
 
+import csv
+import os
+import sys
 import xml.etree.ElementTree as etree
 from collections import namedtuple
 from csv import DictReader
+from os.path import dirname, join
 
 from PIL import Image
 
@@ -18,31 +22,29 @@ def c(rgbhex):
     r, g = divmod(rg, 0x100)
     return (r, g, b)
 
-PIXEL_STATUSES = [          # From the http://www.bonap.org/MapKey.html page:
-    (c(0x008000), True),  # Species present in state and native
-    (c(0x00FF00), True),  # Species present and not rare
-    (c(0xFE0000), False), # Species extinct
-    (c(0x00DD90), True),  # Species native, but adventive in state
-    (c(0x3AB2E6), False), # Species waif; see http://en.wikipedia.org/wiki/Waif
-    (c(0x57A6FF), False), # ANOTHER color for waif? see Galeopsis ladanum
-    (c(0x0000EA), True),  # Species present in state and exotic
-    (c(0xFFFF00), True),  # Species present and rare
-    (c(0xFF00FE), True),  # Species noxious
-    (c(0x000000), False), # Species eradicated
-    (c(0xAD8E00), False), # Species not present in state
-    (c(0xFE9900), False), # Species extirpated (historic)
-    (c(0x00FFFF), True),  # Species exotic and present
-    #(c(0x), True),  # Questionable Presence (cross-hatched)
+PIXEL_STATUSES = [
+    # From the http://www.bonap.org/MapKey.html page:
+    (c(0x008000), 'present|native'),
+    (c(0x00FF00), 'present'),
+    (c(0xFE0000), 'absent|extinct'),
+    (c(0x00DD90), 'present|adventive|native'),
+    (c(0x3AB2E6), 'absent|waif'),
+    (c(0x57A6FF), 'absent|waif'), # another waif color? see Galeopsis ladanum
+    (c(0x0000EA), 'present|exotic'),
+    (c(0xFFFF00), 'present|rare'),
+    (c(0xFF00FE), 'present|noxious'),
+    (c(0x000000), 'absent|eradicated'),
+    (c(0xAD8E00), 'absent'),
+    (c(0xFE9900), 'absent|extirpated'),
+    (c(0x00FFFF), 'present|exotic'),
     ]
 
 del c
 range3 = range(3)
 
 def pixel_status(pixel):
-    #print '---'
     for rgb, status in PIXEL_STATUSES:
         dsq = sum((pixel[i] - rgb[i]) ** 2 for i in range3)
-        #print rgb, status, dsq
         if dsq < 200:
             return status
     raise ValueError('I cannot determine what the pixel {0} means'
@@ -69,34 +71,60 @@ class MapScanner(object):
         self.points.sort()
 
     def scan(self, map_image_path):
-        #print map_image_path
         im = Image.open(map_image_path)
-        if False: #for p in self.points:
-            print '--'
-            print p
-            print im.getpixel((p.x, p.y))
-            print pixel_status(im.getpixel((p.x, p.y)))
         return [
             MapStatus(p.state, p.county, pixel_status(im.getpixel((p.x, p.y))))
             for p in self.points
             ]
 
-if __name__ == '__main__':
-    import os
-    dirname = os.path.dirname
+#
+
+def scan(svg_path, mapdir, bonap_path):
+    ms = MapScanner(svg_path)
+    csv_writer = csv.writer(open(bonap_path, 'wb'))
+    csv_writer.writerow(('scientific_name', 'state', 'county', 'status'))
+    for pngname in os.listdir(mapdir):
+        if not pngname.endswith('.png'):
+            continue
+        scientific_name = pngname[:-4]
+        pngpath = join(mapdir, pngname)
+        for tup in ms.scan(pngpath):
+            row = [scientific_name, tup.state, tup.county, tup.status]
+            csv_writer.writerow(row)
+
+def report():
+    pass
+
+#
+
+def main():
     thisdir = dirname(__file__)
     topdir = dirname(dirname(dirname(dirname(thisdir))))
-    mapdir = os.path.join(topdir, 'kartesz_maps', 'New_England_maps')
-    csvdir = os.path.join(topdir, 'buildout-myplants', 'data')
-    ms = MapScanner(os.path.join(thisdir, 'new-england-counties2.svg'))
+    mapdir = join(topdir, 'kartesz_maps', 'New_England_maps')
+    datadir = join(topdir, 'buildout-myplants', 'data')
+
+    svg_path = join(join(thisdir, 'new-england-counties2.svg'))
+    bonap_path = join(datadir, 'bonap.csv')
+    taxa_path = join(datadir, 'taxa.csv')
+
+    if sys.argv[1:] == ['scan']:
+        scan(svg_path, mapdir, bonap_path)
+    elif sys.argv[1:] == ['report']:
+        report(bonap_path, taxa_path)
+    else:
+        print >>sys.stderr, 'usage: {0} scan|report'.format(sys.argv[0])
+
+if __name__ == '__main__':
+    main()
+
     if False:
         # Simple test; just print out data from one map.
-        pngpath = os.path.join(data, 'Acorus americanus New England.png')
+        pngpath = join(data, 'Acorus americanus New England.png')
         for tup in ms.scan(pngpath):
             print tup
-    if True:
+    if False:
         # Read in taxa.csv and compare.
-        csvpath = os.path.join(csvdir, 'taxa.csv')
+        csvpath = join(csvdir, 'taxa.csv')
         with open(csvpath) as csvfile:
             total = misses = 0
             # go = False
@@ -112,7 +140,7 @@ if __name__ == '__main__':
                 # if not go:
                 #     continue
 
-                pngpath = os.path.join(mapdir, sn + '.png')
+                pngpath = join(mapdir, sn + '.png')
                 if not os.path.exists(pngpath):
                     print 'No map for species {0}'.format(sn)
                     misses += 1
