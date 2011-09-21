45592e3195e1470894e3945743f3306893e6b353
John Galada <jg344@fastmail.net>
2011-09-21 19:25:26 +0000

    On species pages, display the New England distribution and conservation status data in an easy-to-read table with status values now separated into columns.
    git-svn-id: http://svn.jazkarta.com/newfs-database-api/gobotany/trunk/src@1533 fafe9cc9-101d-4c6e-bffa-87022359e7d1

--- gobotany/simplekey/templatetags/simplekey_extras.py
+++ gobotany/simplekey/templatetags/simplekey_extras.py
@@ -3,6 +3,29 @@ from django.template.defaultfilters import stringfilter
 
 register = template.Library()
 
+# "Collect" tag courtesy of http://djangosnippets.org/snippets/2196/
+# Collects items into a list that can be used later in the template.
+@register.tag
+def collect(parser, token):
+    bits = list(token.split_contents())
+    if len(bits) > 3 and bits[-2] == 'as':
+        varname = bits[-1]
+        items = bits[1:-2]
+        return CollectNode(items, varname)
+    else:
+        raise template.TemplateSyntaxError( \
+            '%r expected format is "item [item ...] as varname"' % bits[0])
+
+class CollectNode(template.Node):
+    def __init__(self, items, varname):
+        self.items = map(template.Variable, items)
+        self.varname = varname
+
+    def render(self, context):
+        context[self.varname] = [i.resolve(context) for i in self.items]
+        return ''
+
+
 @stringfilter
 def split(value, arg):
     '''Split a string into a list on the given character.'''
