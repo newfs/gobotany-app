0173700602dcb281f9c07bda02b199722601dcec
Brandon Rhodes <brandon@rhodesmill.org>
2011-10-15 02:53:34 +0000

    Protected the search-suggest JS from running before Dojo gets loaded.
    git-svn-id: http://svn.jazkarta.com/newfs-database-api/gobotany/trunk/src@1578 fafe9cc9-101d-4c6e-bffa-87022359e7d1

--- gobotany/static/scripts/activate_search_suggest.js
+++ gobotany/static/scripts/activate_search_suggest.js
@@ -1,5 +1,13 @@
-dojo.require('gobotany.sk.SearchSuggest');
-dojo.addOnLoad(function() {
-    var search_suggest = gobotany.sk.SearchSuggest('{{ query }}');
-    search_suggest.setup();
+require([
+    'dojo_config',
+    '/static/js/dojo/dojo.js',
+    '/static/js/layers/sk.js'
+], function() {
+
+    dojo.require('gobotany.sk.SearchSuggest');
+    dojo.addOnLoad(function() {
+        var search_suggest = gobotany.sk.SearchSuggest('{{ query }}');
+        search_suggest.setup();
+    });
+
 });
