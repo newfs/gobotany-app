5c005e6ee8e50723546b2e3ad840d275ae8e8de4
John Galada <jg344@fastmail.net>
2011-10-18 14:19:56 +0000

    Add a note about a future fix needed for the search results page.
    git-svn-id: http://svn.jazkarta.com/newfs-database-api/gobotany/trunk/src@1583 fafe9cc9-101d-4c6e-bffa-87022359e7d1

--- gobotany/static/scripts/activate_search_suggest.js
+++ gobotany/static/scripts/activate_search_suggest.js
@@ -6,6 +6,10 @@ require([
 
     dojo.require('gobotany.sk.SearchSuggest');
     dojo.addOnLoad(function() {
+        // TODO: Fix this broken constructor string. It is a holdover from
+        // when this code existed in a template. It worked there, but it
+        // will not work as is once we move the search results page over
+        // to the _new_base.html/require.js system.
         var search_suggest = gobotany.sk.SearchSuggest('{{ query }}');
         search_suggest.setup();
     });
