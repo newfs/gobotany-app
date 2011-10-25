96df089fc3cfd6f292afbe65c3b5e5d62aafffa4
Brandon Rhodes <brandon@rhodesmill.org>
2011-10-25 16:40:12 +0000

    Simplified how RequireJS kicks off the results page: by predefining a tiny module full of constants (like the pile_slug), there is suddenly no need to construct some way of passing a function into the module.  This eliminates a function(){} wrapper and thus a level of indentation!
    git-svn-id: http://svn.jazkarta.com/newfs-database-api/gobotany/trunk/src@1622 fafe9cc9-101d-4c6e-bffa-87022359e7d1

--- gobotany/static/scripts/simplekey/results.js
+++ gobotany/static/scripts/simplekey/results.js
@@ -1,46 +1,41 @@
 define([
-], {
-    /* Since the HTML template is what knows the pile_slug, we provide
-     * it with a function it can invoke that takes the pile_slug as an
-     * argument.
-     */
-    startup: function(pile_slug) {
+    'args'
+], function(args) {
 
-        require([
-            'simplekey/results_overlay'
-        ]);
+    require([
+        'simplekey/results_overlay'
+    ]);
 
-        require([
-            'dojo',
-            'order!jquery.tools.min',  // needed by jscrollpane
-            'order!jquery.jscrollpane.min',  // sk/results.js
-            'underscore-min',  // filters.js, etc
-            'global'  // sk/results.js, etc
-        ], function() {
-            dojo.require('gobotany.sk.results');
-            dojo.addOnLoad(function() {
-                helper = gobotany.sk.results.ResultsHelper(pile_slug);
-                helper.setup();
-            });
+    require([
+        'dojo',
+        'order!jquery.tools.min',  // needed by jscrollpane
+        'order!jquery.jscrollpane.min',  // sk/results.js
+        'underscore-min',  // filters.js, etc
+        'global'  // sk/results.js, etc
+    ], function() {
+        dojo.require('gobotany.sk.results');
+        dojo.addOnLoad(function() {
+            helper = gobotany.sk.results.ResultsHelper(args.pile_slug);
+            helper.setup();
         });
+    });
 
-        /* Hook up our gallery applications. */
+    /* Hook up our gallery applications. */
 
-        require([
-            'activate_image_gallery',
-            'activate_search_suggest',
-            'shadowbox'
-        ], function() {
-            Shadowbox.init({
-                onOpen: function() {
-                    // Move the Shadowbox close link.
-                    var tb = document.getElementById('sb-wrapper');
-                    if (tb) {
-                        var snc = document.getElementById('sb-nav-close');
-                        tb.appendChild(snc);
-                    }
+    require([
+        'activate_image_gallery',
+        'activate_search_suggest',
+        'shadowbox'
+    ], function() {
+        Shadowbox.init({
+            onOpen: function() {
+                // Move the Shadowbox close link.
+                var tb = document.getElementById('sb-wrapper');
+                if (tb) {
+                    var snc = document.getElementById('sb-nav-close');
+                    tb.appendChild(snc);
                 }
-            });
+            }
         });
-    }
+    });
 });
