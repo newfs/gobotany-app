6ba3f86ac5287406641b029f76a6dde25c3ae5c5
Brandon Rhodes <brandon@rhodesmill.org>
2011-11-28 18:30:13 +0000

    Restored search-suggest on the Level 1 and Level 2 pages.
    git-svn-id: http://svn.jazkarta.com/newfs-database-api/gobotany/trunk/src@1738 fafe9cc9-101d-4c6e-bffa-87022359e7d1

--- gobotany/static/scripts/activate_search_suggest.js
+++ gobotany/static/scripts/activate_search_suggest.js
@@ -1,16 +1,13 @@
 require([
     'dojo_config',
+    'simplekey/resources',
     '/static/js/dojo/dojo.js',
     '/static/js/layers/sk.js'
 ], function() {
 
     dojo.require('gobotany.sk.SearchSuggest');
     dojo.addOnLoad(function() {
-        // TODO: Fix this broken constructor string. It is a holdover from
-        // when this code existed in a template. It worked there, but it
-        // will not work as is once we move the search results page over
-        // to the _new_base.html/require.js system.
-        var search_suggest = gobotany.sk.SearchSuggest('{{ query }}');
+        var search_suggest = gobotany.sk.SearchSuggest();
         search_suggest.setup();
     });
 
