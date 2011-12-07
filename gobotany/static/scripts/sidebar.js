1a9ff7c9e289a571b381a06b4fe5cc2aace8dc61
John Galada <jg344@fastmail.net>
2011-12-07 20:48:36 +0000

    Fix bug where sidebar height was no longer being set on the search results page. Add a new sidebar script to start the transition away from global.js.
    git-svn-id: http://svn.jazkarta.com/newfs-database-api/gobotany/trunk/src@1763 fafe9cc9-101d-4c6e-bffa-87022359e7d1

---
+++ gobotany/static/scripts/sidebar.js
@@ -0,0 +1,37 @@
+define(['jquery.tools.min'], function() {
+
+    // Make the sidebar as tall as it can be.
+    sidebar_set_height = function() {
+        var MINIMUM_HEIGHT = 550;
+        var new_height = 0;
+
+        var main_height = $('div#main').height();
+        if (main_height > new_height) {
+            new_height = main_height;
+        }
+
+        // Handle cases where the sidebar is taller than the main content.
+        // Because the sidebar is usually set to a static height when the
+        // page loads, its height cannot be trusted as accurate (hence this
+        // function). So, tally the heights of all the items in the sidebar.
+        var SIDEBAR_SECTION_VERTICAL_PAD = 16;
+        var sidebar_child_nodes = $('div#sidebar').children();
+        var sidebar_contents_height = 0;
+        for (var i = 0; i < sidebar_child_nodes.length; i++) {
+            var section_height = $(sidebar_child_nodes[i]).height() +
+                SIDEBAR_SECTION_VERTICAL_PAD;
+            sidebar_contents_height += section_height;
+        }
+        if (sidebar_contents_height > new_height) {
+            new_height = sidebar_contents_height;
+        }
+
+        if (new_height < MINIMUM_HEIGHT) {
+            new_height = MINIMUM_HEIGHT;
+        }
+
+        $('div#sidebar').css('height', new_height);
+    };
+
+});
+
--- gobotany/static/scripts/simplekey/search.js
+++ gobotany/static/scripts/simplekey/search.js
@@ -1,5 +1,8 @@
 /* Resources that search.html needs. */
 
 require([
-    'activate_search_suggest'
-]);
+    'activate_search_suggest',
+    'sidebar'
+], function() {
+    sidebar_set_height();   
+});
