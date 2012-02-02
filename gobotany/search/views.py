5e68e303c4d158d9d8d544b24c3e27aef5b22e3e
John Galada <jg344@fastmail.net>
2012-02-02 20:03:22 +0000

    Fix bug where searches return no results unless a match is found for every single search term.
    git-svn-id: http://svn.jazkarta.com/newfs-database-api/gobotany/trunk/src@1888 fafe9cc9-101d-4c6e-bffa-87022359e7d1

--- gobotany/search/views.py
+++ gobotany/search/views.py
@@ -15,3 +15,32 @@ class GoBotanySearchView(SearchView):
 
         return extra
 
+    def get_results(self):
+        """When no results are found for multiple word searches, try
+        searching without some of the words in order to be able to
+        return something to the user rather than nothing. This is
+        in the absence of partial word searches (user types a word
+        and a partial word, and the partial word matches using a
+        wildcard). Solr can be made to do partial word searches, but
+        a quick fix was desired instead.
+        """
+        results = self.form.search()
+
+        if len(results) == 0:
+            # Query words come back "cleaned" from get_query().
+            query_words = self.get_query().split(' ')
+            if len(query_words) > 1:
+                # Try queries that drop a word at a time off the end.
+                for end_index in reversed(range(1, len(query_words))):
+                    new_query = ' '.join(query_words[0:end_index])
+                    self.form.cleaned_data['q'] = new_query
+                    results = self.form.search()
+                    if len(results) > 0:
+                        # Found results for one of the words.
+                        # Set our SearchView's query to the altered
+                        # query, so the user can see the shortened
+                        # search query that returned some results.
+                        self.query = self.get_query()
+                        break
+
+        return results
