e6b445e2d69e93fcb8669c996e127336b7ee085e
Brandon Rhodes <brandon@rhodesmill.org>
2012-02-20 12:36:19 +0000

    Told Django to serve static files even in production.
    git-svn-id: http://svn.jazkarta.com/newfs-database-api/gobotany/trunk/src@2021 fafe9cc9-101d-4c6e-bffa-87022359e7d1

--- gobotany/urls.py
+++ gobotany/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.defaults import include, patterns, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 handler404 = 'django.views.defaults.page_not_found'
 handler500 = 'django.views.defaults.server_error'
@@ -16,4 +17,7 @@ urlpatterns = patterns(
     url(r'^plantoftheday/', include('gobotany.plantoftheday.urls')),
     url(r'^tinymce/', include('tinymce.urls')),
     url(r'^', include('gobotany.simplekey.urls')),
-    ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    )
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
