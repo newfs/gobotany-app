4cca76789305d6a660aa652c3573f214bc6acc99
JR Rickerson <jrrickerson@gmail.com>
2012-03-05 15:37:41 -0500

    Force dojo to be loaded before sk.js.
    This should prevent the intermittent problems seen when a script-loading
    race condition causes the page to hang, as seen in Issue #4 (Bug 56).

--- gobotany/static/scripts/activate_search_suggest.js
+++ gobotany/static/scripts/activate_search_suggest.js
@@ -1,16 +1,20 @@
 require([
     'dojo_config',
     'simplekey/resources',
-    '/static/js/dojo/dojo.js',
-    '/static/js/layers/sk.js'
+    '/static/js/dojo/dojo.js'
 ], function() {
+    
+    require([
+        '/static/js/layers/sk.js'
+    ], function() {
 
-    dojo.require('gobotany.sk.SearchSuggest');
-    dojo.addOnLoad(function() {
-        var initial_search_box_value = $('#search input').val();
-        var search_suggest = gobotany.sk.SearchSuggest(
-            initial_search_box_value);
-        search_suggest.setup();
+        dojo.require('gobotany.sk.SearchSuggest');
+        dojo.addOnLoad(function() {
+            var initial_search_box_value = $('#search input').val();
+            var search_suggest = gobotany.sk.SearchSuggest(
+                initial_search_box_value);
+            search_suggest.setup();
+        });
     });
 
 });
