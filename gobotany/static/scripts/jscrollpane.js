551f4ea878d388d3bc13191a8a89f67025c20375
JR Rickerson <jrrickerson@gmail.com>
2012-03-06 15:46:58 -0500

    Add a wrapper script for jquery.jscrollpane.
    Declare jquery as a requirement to prevent race conditions.

---
+++ gobotany/static/scripts/jscrollpane.js
@@ -0,0 +1,7 @@
+// Require.js wrapper for jquery.jscrollpane.min plugin,
+// to ensure proper dependency loading.
+define(['jquery.tools.min'], function() {
+    require([
+        'jquery.jscrollpane.min'
+    ]);
+});
