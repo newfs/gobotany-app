78c2cacff6bd22dc3a490fe6272643520bdde2c8
JR Rickerson <jrrickerson@gmail.com>
2012-03-06 16:40:31 -0500

    Add a wrapper script for jquery.mousewheel.
    Ensure jquery is a dependency to prevent race conditions.

---
+++ gobotany/static/scripts/mousewheel.js
@@ -0,0 +1,8 @@
+// Require.js wrapper for jquery.mousewheel.min plugin,
+// to ensure proper dependency loading.
+define(['jquery.tools.min'], function() {
+    require([
+        'jquery.mousewheel.min'
+    ]);
+});
+
