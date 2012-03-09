6cb61c4ea7863a885259cc8115d13113a46234d4
JR Rickerson <jrrickerson@gmail.com>
2012-03-09 15:49:14 -0500

    Add require.js wrapper for smoothDivScroll plugin.

---
+++ gobotany/static/scripts/smoothDivScroll.js
@@ -0,0 +1,10 @@
+// Require.js wrapper for jquery.smoothDivScroll plugin
+// to ensure proper dependency loading.
+define(['jquery-ui-custom'], function() {
+    require([
+        'jquery.smoothDivScroll-1.1-mod' /* un-minified, with bugfix by
+                                            Go Botany */
+    ]);
+});
+
+
