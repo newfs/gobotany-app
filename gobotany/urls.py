102182fdb41109cb4ef04da5d4d53ab2e7d36c9f
Brandon Rhodes <brandon@rhodesmill.org>
2012-04-03 12:11:22 -0400

    Removed two old import statements that are no longer needed.

--- gobotany/urls.py
+++ gobotany/urls.py
@@ -1,6 +1,4 @@
-from django.conf import settings
 from django.conf.urls.defaults import include, patterns, url
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
