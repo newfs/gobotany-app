95140cf1fcb8963fe25cca519dcdbdc5a9b263ab
John Galada <jg344@fastmail.net>
2012-04-16 11:38:10 -0400

    On iOS, make video links open in the device's YouTube app.
    Trying to use Shadowbox for videos on iOS is problematic: different behavior
    on iOS 4 vs. 5, needing to scroll to the top for the video to play, and
    unreliable playback ability (sometimes bumping the scroll helps). So,
    use the reliable YouTube app on the device, even though the user will
    have to switch back to Safari after watching the video.

--- gobotany/static/scripts/activate_video_links.js
+++ gobotany/static/scripts/activate_video_links.js
@@ -6,22 +6,28 @@ require([
     'shadowbox_init'
 ], function() {
     $(document).ready(function() {
-        $('a.video').click(function() {
-            // Work around a bug when using lightboxes on iOS:
-            // Videos do not start playing if the page is scrolled down,
-            // so scroll to the top.
+        $('a.video').each(function() {
+            // On iOS, instead of trying to use the lightbox for videos
+            // (due to buggy behavior that requires scrolling to the top),
+            // use a link that will open in the device's YouTube app.
             if (navigator.userAgent.match(/(iPad|iPod|iPhone)/)) {
-                if (this.href.indexOf('youtube.com') > -1) {
-                    window.scrollTo(0, 0);
-                }
+                var start = this.href.lastIndexOf('/') + 1;
+                var end = this.href.indexOf('?');
+                var video_id = this.href.substring(start, end);
+                var youtube_app_url = 'http://www.youtube.com/v/' + video_id;
+                this.href = youtube_app_url;
             }
-
-            Shadowbox.open({
-                content: this.href,
-                player: "iframe"
-            });
-            return false;
+            else {
+                // Open the video in a lightbox.
+                var link = this;
+                $(this).click(function() {
+                    Shadowbox.open({
+                        content: link.href,
+                        player: "iframe"
+                    });
+                    return false;
+                });
+            }
         });
     });
 });
-
