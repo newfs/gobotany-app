5152b07c71fc86840bfe37015037daa7a41fbdd1
JR Rickerson <jrrickerson@gmail.com>
2012-04-23 17:26:07 -0400

    Add configuration for Dojo 1.7

--- gobotany/static/scripts/dojo_config.js
+++ gobotany/static/scripts/dojo_config.js
@@ -7,5 +7,25 @@ djConfig = {
         gobotany: '/static/gobotany',
         layers: '/static/js/layers'
     },
-    parseOnLoad: true
+    parseOnLoad: true,
+    // Config for 1.7, using backward compatibility config object
+    async: false
+    packages: [
+        { name: 'dojo', location: 'js/lib/dojo' },
+        { name: 'dojox', location: 'js/lib/dojox' },
+    ]
 };
+
+// New Dojo 1.7 global configuration object
+// This must be loaded before the dojo AMD loader
+dojoConfig = {
+    isDebug: false,
+    baseUrl: '/static/',
+    // Use Legacy loader mode until we finish migration
+    async: false,
+    packages: [
+        { name: 'dojo', location: 'js/lib/dojo' },
+        { name: 'dojox', location: 'js/lib/dojox' },
+    ],
+};
+
