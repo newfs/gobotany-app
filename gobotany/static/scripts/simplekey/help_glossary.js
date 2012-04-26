5d498db0c5e73f3e2614cc288963d08cacb72c8b
Brandon Rhodes <brandon@rhodesmill.org>
2012-04-26 16:46:35 -0400

    Got glossary-term highlighting working again on the glossary pages.

--- gobotany/static/scripts/simplekey/help_glossary.js
+++ gobotany/static/scripts/simplekey/help_glossary.js
@@ -1,35 +1,14 @@
 require([
     'activate_search_suggest',
     'shadowbox',
-    'shadowbox_init'
+    'shadowbox_init',
+    'sidebar'
 ]);
 
 require([
-    'dojo_config',
-    '/static/js/dojo/dojo.js',
-    '/static/js/layers/sk.js',
-    'lib/tooltipsy',
-    'sidebar'
-], function() {
-
-    dojo.require('gobotany.sk.glossary');
-
-dojo.declare('gobotany.sk.glossary.GlossaryHelper', null, {
-    constructor: function() {
-        this.glossarizer = new gobotany.sk.glossary.Glossarizer();
-    },
-    setup: function() {
-        var glossarizer = this.glossarizer;
-        $('#terms dd').each(function(i, node) {
-            glossarizer.markup(node);
-        });
-    }
-});
-
-
-    dojo.addOnLoad(function() {
-        glossary_helper = gobotany.sk.glossary.GlossaryHelper();
-        glossary_helper.setup();
+    'simplekey/glossarize'
+], function(glossarize) {
+    $(document).ready(function() {
+        glossarize($('#terms dd'));
     });
-
 });
