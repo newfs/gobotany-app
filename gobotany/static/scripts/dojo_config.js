dc63256711ec515befd367b5b169f86f708df79c
JR Rickerson <jrrickerson@gmail.com>
2012-04-27 12:55:46 -0400

    Fix paths to dojo packages, turn on parseOnLoad.

--- gobotany/static/scripts/dojo_config.js
+++ gobotany/static/scripts/dojo_config.js
@@ -7,25 +7,23 @@ djConfig = {
         gobotany: '/static/gobotany',
         layers: '/static/js/layers'
     },
-    parseOnLoad: true,
-    // Config for 1.7, using backward compatibility config object
-    async: false,
-    packages: [
-        { name: 'dojo', location: 'js/lib/dojo' },
-        { name: 'dojox', location: 'js/lib/dojox' },
-    ]
+    parseOnLoad: true
 };
 
 // New Dojo 1.7 global configuration object
 // This must be loaded before the dojo AMD loader
 dojoConfig = {
     isDebug: false,
-    baseUrl: '/static/',
-    // Use Legacy loader mode until we finish migration
+    baseUrl: '/static/scripts',
+    tlmSiblingOfDojo: false,
     async: false,
     packages: [
-        { name: 'dojo', location: 'js/lib/dojo' },
-        { name: 'dojox', location: 'js/lib/dojox' },
+        { name: 'dojo', location: 'lib/dojo' },
+        { name: 'dojox', location: 'lib/dojox' },
+        { name: 'gobotany', location: '/static/gobotany' }
     ],
+    // Until we fix the rest of the AMD modules
+    // we need to leave this as true
+    parseOnLoad: true
 };
 
