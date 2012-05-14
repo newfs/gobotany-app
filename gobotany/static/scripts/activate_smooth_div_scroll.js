66c5fca0abc1b75dc8d9942ec466adf4a7c2239b
Brandon Rhodes <brandon@rhodesmill.org>
2012-05-14 16:41:35 -0400

    Fixed #118 by upgrading to smoothDivScroll 1.2, so that more than one image now shows up in the species-page image gallery.

--- gobotany/static/scripts/activate_smooth_div_scroll.js
+++ gobotany/static/scripts/activate_smooth_div_scroll.js
@@ -4,17 +4,26 @@
  */
 
 require([
-    'jquery.tools.min',
+    'jquery.tools.min'
 ], function() {
     require([
-        'jquery-ui-1.8.16.custom.min',
+        'jquery-ui-1.8.16.custom.min'
     ], function() {
         require([
-            'jquery.smoothDivScroll-1.1-mod', /* un-minified, with bugfix by
-                                                 Go Botany */
+            'lib/jquery.smoothDivScroll-1.2' /* un-minified, with bugfix by
+                                                Go Botany */
         ], function() {
             $(document).ready(function() {
-                $('#species-images').smoothDivScroll({
+
+               // Even though we leave mousewheeling turned off, the
+               // smoothDivScroll will still try to register an event
+               // handler, so we create a fake widget to avoid an error.
+
+               $.widget('fake.mousewheel', {});
+
+               // Activate!
+
+               $('#species-images').smoothDivScroll({
                     autoScroll: 'onstart', 
                     autoScrollDirection: 'backandforth', 
                     autoScrollStep: 1, 
