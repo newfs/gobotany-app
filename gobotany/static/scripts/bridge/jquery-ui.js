8e89eb479fc9afd52410d79215983434b12ee2fb
JR Rickerson <jrrickerson@gmail.com>
2012-05-22 15:21:28 -0400

    Fix type in declared module name.

--- gobotany/static/scripts/bridge/jquery-ui.js
+++ gobotany/static/scripts/bridge/jquery-ui.js
@@ -4,7 +4,7 @@
 // has jquery loaded before it attempts to load. This also serves
 // as a thin abstraction layer so we don't have to worry about
 // versioned filenames in our module references.
-define('jquery.cookie', [
+define('jquery-ui', [
     'bridge/jquery', 
     'jquery/jquery-ui-1.8.16.custom.min'
 ], function($, ui) {
