9ff83df9d885626738436c073e66a46e76bd7932
JR Rickerson <jrrickerson@gmail.com>
2012-05-22 14:42:46 -0400

    Wrap and update references to jquery.easing plugin.
    As a consequence of creating this wrapper, create a version-less wrapper
    for jquery-ui library as well to ensure it's loaded before the plugin.

--- gobotany/static/scripts/activate_smooth_div_scroll.js
+++ gobotany/static/scripts/activate_smooth_div_scroll.js
@@ -4,34 +4,31 @@
  */
 
 require([
-    'bridge/jquery'
-], function($) {
+    'bridge/jquery',
+    'bridge/jquery-ui',
+    'bridge/jquery.easing',
+], function($, ui, easing) {
     require([
-        'jquery-ui-1.8.16.custom.min',
-        'lib/jquery.easing.1.3'
+        'lib/jquery.smoothDivScroll-1.2-mod' // un-minified, with bugfix
+                                             // by Go Botany
     ], function() {
-        require([
-            'lib/jquery.smoothDivScroll-1.2-mod' // un-minified, with bugfix
-                                                 // by Go Botany
-        ], function() {
-            $(document).ready(function() {
+        $(document).ready(function() {
 
-               // Even though we leave mousewheeling turned off, the
-               // smoothDivScroll will still try to register an event
-               // handler, so we create a fake widget to avoid an error.
+           // Even though we leave mousewheeling turned off, the
+           // smoothDivScroll will still try to register an event
+           // handler, so we create a fake widget to avoid an error.
 
-               $.widget('fake.mousewheel', {});
+           $.widget('fake.mousewheel', {});
 
-               // Activate!
-               
-               $('#species-images').smoothDivScroll({
-                   autoScrollingMode: 'onstart', 
-                   autoScrollingDirection: 'backandforth', 
-                   autoScrollingStep: 1, 
-                   autoScrollingInterval: 75,
-                   visibleHotSpotBackgrounds: 'always'
-               });
-            });
+           // Activate!
+           
+           $('#species-images').smoothDivScroll({
+               autoScrollingMode: 'onstart', 
+               autoScrollingDirection: 'backandforth', 
+               autoScrollingStep: 1, 
+               autoScrollingInterval: 75,
+               visibleHotSpotBackgrounds: 'always'
+           });
         });
     });
 });
---
+++ gobotany/static/scripts/bridge/jquery-ui.js
@@ -0,0 +1,20 @@
+// AMD wrapper for jquery-ui library.
+// This should ensure the AMD loader properly caches jquery
+// and only loads it once, and we can be sure that this library
+// has jquery loaded before it attempts to load. This also serves
+// as a thin abstraction layer so we don't have to worry about
+// versioned filenames in our module references.
+define('jquery.cookie', [
+    'bridge/jquery', 
+    'jquery/jquery-ui-1.8.16.custom.min'
+], function($, ui) {
+    var jquery;
+    if($.ui) {
+        // The plugin is loaded, so return the same
+        // jQuery object again (the global)
+        jquery = $;
+    }
+
+    return jquery;
+});
+
