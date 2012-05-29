8941226b854f0440970f5ff7efc30eb11950408d
JR Rickerson <jrrickerson@gmail.com>
2012-05-29 15:32:15 -0400

    Remove use of the inline "args" module in our templates.
    To avoid having to include the html templates in our build configuration
    becaue they define their own inline modules, modify the modules which
    require an args object to return a function rather than executing when
    parsed.  Then we can simply create a plain old Javascript object in the
    html template and pass it into that function as a regular parameter.

--- gobotany/static/scripts/simplekey/family.js
+++ gobotany/static/scripts/simplekey/family.js
@@ -1,26 +1,28 @@
 define([
-    'args',
     'bridge/jquery',
     'simplekey/glossarize'
-], function(args, $, glossarize) {
+], function($, glossarize) {
 
-    $(document).ready(function() {
-        glossarize($('.description'));
-    });
-
-    require([
-        'util/activate_search_suggest',
-        'bridge/shadowbox',
-        'util/shadowbox_init'
-    ]);
-
-    require([
-        'util/sidebar'
-    ], function() {
-        dojo.require('gobotany.sk.family');
-        dojo.addOnLoad(function() {
-            gobotany.sk.family.init(args.family_slug);
+    var module_function = function(args) {
+        $(document).ready(function() {
+            glossarize($('.description'));
         });
-    });
 
+        require([
+            'util/activate_search_suggest',
+            'bridge/shadowbox',
+            'util/shadowbox_init'
+        ]);
+
+        require([
+            'util/sidebar'
+        ], function() {
+            dojo.require('gobotany.sk.family');
+            dojo.addOnLoad(function() {
+                gobotany.sk.family.init(args.family_slug);
+            });
+        });
+    };
+
+    return module_function;
 });
--- gobotany/static/scripts/simplekey/genus.js
+++ gobotany/static/scripts/simplekey/genus.js
@@ -1,27 +1,29 @@
 define([
-    'args',
     'bridge/jquery',
     'simplekey/glossarize'
-], function(args, $, glossarize) {
-
-    $(document).ready(function() {
-        glossarize($('.description'));
-    });
-
-    require([
-        'util/activate_search_suggest',
-        'bridge/shadowbox',
-        'util/shadowbox_init'
-    ]);
-
-    require([
-        'util/sidebar'
-    ], function() {
-        dojo.require('gobotany.sk.genus');
-        dojo.addOnLoad(function() {
-            gobotany.sk.genus.init(args.genus_slug);
+], function($, glossarize) {
+    
+    var module_function = function(args) {
+        $(document).ready(function() {
+            glossarize($('.description'));
         });
-    });
 
+        require([
+            'util/activate_search_suggest',
+            'bridge/shadowbox',
+            'util/shadowbox_init'
+        ]);
+
+        require([
+            'util/sidebar'
+        ], function() {
+            dojo.require('gobotany.sk.genus');
+            dojo.addOnLoad(function() {
+                gobotany.sk.genus.init(args.genus_slug);
+            });
+        });
+    };
+
+    return module_function;
 });
 
