047f63447bc36e29369124dabab907b287176fdf
John Galada <jg344@fastmail.net>
2012-06-11 11:19:07 -0400

    Species page: italicize plant names in the Notes sidebar section.
    Extend the code that italicizes plant names to deal with the few bits of
    surrounding text that can appear in this field as well as the occasional
    multiple sentences that can also appear there. (#149)

--- gobotany/simplekey/templatetags/simplekey_extras.py
+++ gobotany/simplekey/templatetags/simplekey_extras.py
@@ -1,3 +1,5 @@
+import re
+
 from django import template
 from django.template.defaultfilters import stringfilter
 
@@ -45,53 +47,88 @@ def replace(value, arg):
 
 
 def _italicize_word(word):
-    return '<i>' + word + '</i>'
+    """Italicize a word in HTML, unless already done."""
+    if word.find('<i>') == -1:
+        return '<i>' + word + '</i>'
+    else:
+        return word
 
 @stringfilter
 def italicize_plant(value):
-    """Italicize the latin parts of a plant scientific name."""
-    words = value.split(' ')
+    """Italicize the latin parts of a plant scientific name.
+    Try to handle a bit of surrounding text to allow this filter to
+    be used in other situations, such as the 'Notes on Subspecies
+    and Varieties' on the species page.
+    """
 
-    # Join any false word breaks resulting from HTML span tags added
-    # for search result highlighting.
-    fixed_words = []
-    for count, word in enumerate(words):
-        # If the word contains an opening HTML span tag, join it with
-        # the next word, which will contain the closing HTML span tag.
-        # (A pair of highlight span tags only surrounds a single word.)
-        if word.find('<span') > -1:
-            try:
-                new_word = word + ' ' + words[count + 1]
-            except IndexError:
-                pass
-        elif word.find('</span>') > -1:
-            # If it contains a closing HTML span tag, ignore it because
-            # it has already been joined with the opening tag.
-            continue
-        else:
-            new_word = word
-        fixed_words.append(new_word)
-    words = fixed_words
+    # Support for sentences is limited to the ones from 'Notes on
+    # Subspecies and Varieties.' These end with a New England state
+    # abbreviation followed by a period.
+    sentence_regex = re.compile(r'(.*(?:CT|MA|ME|NH|RI|VT)\.)')
+    sentences = sentence_regex.split(value)
+    sentences = [sentence.strip() for sentence in sentences
+                 if len(sentence.strip()) > 0]
 
-    if len(words) >= 2:
-        # Usually the first two words are the genus and specific epithet.
-        if words[0] != 'Genus:':
-            words[0] = _italicize_word(words[0])
-        words[1] = _italicize_word(words[1])
-        # Look for more words to italicize, such as epithets in a variety
-        # or subspecies name.
-        CONNECTING_TERMS = ['subsp.', 'ssp.', 'var.', 'subvar.', 'f.',
-                            'forma', 'subf.']
-        for i in range(2, len(words)):
-            try:
-                if words[i] in CONNECTING_TERMS:
-                    # Check also for whether the next word is a
-                    # connecting term, such as with "L. f. var.".
-                    if words[i + 1] not in CONNECTING_TERMS:
-                        words[i + 1] = _italicize_word(words[i + 1])
-            except IndexError:
-                pass
-    return ' '.join(words)
+    new_sentences = []
+    for sentence in sentences:
+        words = sentence.split(' ')
+
+        # Join any false word breaks resulting from HTML span tags added
+        # for search result highlighting.
+        fixed_words = []
+        for count, word in enumerate(words):
+            # If the word contains an opening HTML span tag, join it with
+            # the next word, which will contain the closing HTML span tag.
+            # (A pair of highlight span tags only surrounds a single word.)
+            if word.find('<span') > -1:
+                try:
+                    new_word = word + ' ' + words[count + 1]
+                except IndexError:
+                    pass
+            elif word.find('</span>') > -1:
+                # If it contains a closing HTML span tag, ignore it because
+                # it has already been joined with the opening tag.
+                continue
+            else:
+                new_word = word
+            fixed_words.append(new_word)
+        words = fixed_words
+
+        if len(words) >= 2:
+            # Usually the first two words are the genus and specific
+            # epithet, but there are a few special cases to handle.
+            remaining_start = 2
+            if words[0] == 'Genus':
+                words[1] = _italicize_word(words[1])
+            elif ' '.join(words[0:3]) in ['Our subspecies is',
+                                          'Our variety is']:
+                words[3] = _italicize_word(words[3])
+                words[4] = _italicize_word(words[4])
+                remaining_start = 5
+            elif ' '.join(words[0:5]) == 'Our subspecies and variety is':
+                words[6] = _italicize_word(words[6])
+                words[7] = _italicize_word(words[7])
+                remaining_start = 8
+            else:
+                words[0] = _italicize_word(words[0])
+                words[1] = _italicize_word(words[1])
+
+            # Look for more words to italicize, such as epithets in a variety
+            # or subspecies name.
+            CONNECTING_TERMS = ['subsp.', 'ssp.', 'var.', 'subvar.', 'f.',
+                                'forma', 'subf.']
+            for i in range(remaining_start, len(words)):
+                try:
+                    if words[i] in CONNECTING_TERMS:
+                        # Check also for whether the next word is a
+                        # connecting term, such as with "L. f. var.".
+                        if words[i + 1] not in CONNECTING_TERMS:
+                            words[i + 1] = _italicize_word(words[i + 1])
+                except IndexError:
+                    pass
+        new_sentences.append(' '.join(words))
+
+    return ' '.join(new_sentences)
 
 
 # Range filter courtesy of http://djangosnippets.org/snippets/1357/
