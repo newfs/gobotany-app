d1d21dfbe1d67e3747f89a5e7439fdf9a4d69b25
JR Rickerson <jrrickerson@gmail.com>
2012-06-19 12:19:39 -0400

    Extract SpeciesCounts from SpeciesSectionHelper.
    Additionally transform SpeciesCounts into an AMD module, removing legacy
    dojo calls, and references to globals.

--- gobotany/static/gobotany/sk/SpeciesSectionHelper.js
+++ gobotany/static/gobotany/sk/SpeciesSectionHelper.js
@@ -694,32 +694,5 @@ return declare('gobotany.sk.SpeciesSectionHelper', null, {
 
 });
 
-/*
- * Manage everywhere on the page that we maintain a species count.
- */
-dojo.declare('gobotany.sk.SpeciesCounts', null, {
-    animation: null,
-
-    _update_counts: function(species_list) {
-        App3.taxa.set('len', species_list.length);
-
-        if (this.animation !== null)
-            this.animation.stop();
-
-        var span = dojo.query('.species-count-heading > span');
-        this.animation = span.animateProperty({
-            duration: 2000,
-            properties: {
-                backgroundColor: {
-                    start: '#FF0',
-                    end: '#F0F0C0'
-                }
-            }
-        });
-        this.animation.play();
-    }
-});
-
-return gobotany.sk.SpeciesSectionHelper;
 });
 
