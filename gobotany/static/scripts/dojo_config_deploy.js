592c41998d138ef891a727fae768488c81edac9c
JR Rickerson <jrrickerson@gmail.com>
2012-06-26 16:56:17 -0400

    Use async loading in deployment configuration.

--- gobotany/static/scripts/dojo_config_deploy.js
+++ gobotany/static/scripts/dojo_config_deploy.js
@@ -4,7 +4,7 @@ dojoConfig = {
     isDebug: false,
     baseUrl: '/static/build/scripts',
     tlmSiblingOfDojo: false,
-    async: false,
+    async: true,
     locale: 'en-us',
     packages: [
         { name: 'dojo', location: 'dojo' },
@@ -16,9 +16,6 @@ dojoConfig = {
         { name: 'simplekey', location: 'simplekey' },
         { name: 'bridge', location: 'bridge' },
         { name: 'util', location: 'util' }
-    ],
-    // Until we fix the rest of the AMD modules
-    // we need to leave this as true
-    parseOnLoad: true
+    ]
 };
 
