be3a518a0f9057cc3fd2d0924aa9d206376f291f
John Galada <jg344@fastmail.net>
2012-07-10 11:38:32 -0400

    Species page: fix code indent. (#171)

--- gobotany/static/scripts/util/activate_smooth_div_scroll.js
+++ gobotany/static/scripts/util/activate_smooth_div_scroll.js
@@ -11,19 +11,19 @@ require([
 ], function($, ui, easing, smoothDivScroll) {
     $(document).ready(function() {
 
-       // Activate!
+        // Activate!
        
-       $('#species-images').smoothDivScroll({
-           autoScrollingMode: 'onstart', 
-           autoScrollingDirection: 'backandforth', 
-           autoScrollingStep: 1, 
-           autoScrollingInterval: 75,
-           visibleHotSpotBackgrounds: 'always'
-       });
+        $('#species-images').smoothDivScroll({
+            autoScrollingMode: 'onstart', 
+            autoScrollingDirection: 'backandforth', 
+            autoScrollingStep: 1, 
+            autoScrollingInterval: 75,
+            visibleHotSpotBackgrounds: 'always'
+        });
     
-       // Manually fire the window.load event in order to start
-       // autoscrolling. (It used to work without this before some code
-       // reorganization, so perhaps it can be removed at some point.)
-       $(window).load();
+        // Manually fire the window.load event in order to start
+        // autoscrolling. (It used to work without this before some code
+        // reorganization, so perhaps it can be removed at some point.)
+        $(window).load();
     });
 });
