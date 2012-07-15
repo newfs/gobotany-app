795dcf3aad9c272e40791a5848301e7a5fa9b86e
Brandon Rhodes <brandon@rhodesmill.org>
2012-07-15 19:59:09 -0400

    Translated the FilterSectionHelper to jQuery #166

--- gobotany/static/gobotany/sk/FilterSectionHelper.js
+++ gobotany/static/gobotany/sk/FilterSectionHelper.js
@@ -1,26 +1,22 @@
 define([
     'dojo/_base/declare',
-    'dojo/_base/lang',
-    'dojo/query',
-    'dojo/dom-construct',
     'bridge/jquery'
-], function(declare, lang, query, domConstruct,
-            $) {
+], function(declare, $) {
 return declare('gobotany.sk.FilterSectionHelper', null, {
 
     _setup_character_groups: function(character_groups) {
-        console.log('FilterSectionHelper: Updating character groups');
-
-        var character_groups_list = query('ul.char-groups')[0];
-        domConstruct.empty(character_groups_list);
-        var i;
-        for (i = 0; i < character_groups.length; i++) {
-            var character_group = character_groups[i];
-            var item = domConstruct.create('li', { innerHTML: '<label>' +
-                '<input type="checkbox" value="' + character_group.id +
-                '"> ' + character_group.name + '</label>'});
-            domConstruct.place(item, character_groups_list);
-        }
+        var $ul = $('ul.char-groups').empty();
+        _.each(character_groups, function(character_group) {
+            $ul.append(
+                $('<li>').append(
+                    $('<label>').append(
+                        $('<input>', {type: 'checkbox',
+                                      value: character_group.id}),
+                        ' ' + character_group.name
+                    )
+                )
+            );
+        });
     }
 });
 });
