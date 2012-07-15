765fe23603918aace8336dfb73a1c8515983702c
Brandon Rhodes <brandon@rhodesmill.org>
2012-07-14 22:27:57 -0400

    Merged sk/genus.js into the main genus.js #166

--- gobotany/static/scripts/simplekey/genus.js
+++ gobotany/static/scripts/simplekey/genus.js
@@ -1,21 +1,48 @@
 define([
     'bridge/jquery',
-    'gobotany/sk/genus',
+    'gobotany/sk/PhotoHelper',
     'util/sidebar',
     'util/activate_search_suggest',
     'util/shadowbox_init',
     'simplekey/glossarize'
-], function($, genus, sidebar, activate_search_suggest, shadowbox_init,
-    glossarize) {
-    
-    var module_function = function(args) {
-        $(document).ready(function() {
-            glossarize($('.description'));
-            sidebar.setup();
-            genus.init(args.genus_slug);
+], function($, PhotoHelper, sidebar, activate_search_suggest, shadowbox_init,
+            glossarize) {
+
+    var genus = {};
+
+    var _setup_page = function() {
+        glossarize($('.description'));
+        sidebar.setup();
+
+        var photo_helper = PhotoHelper();
+
+        // Wire up each image link to a Shadowbox popup handler.
+        var $images = $('.pics .plant');
+        $images.each(function(i, plant_image_div) {
+            var frame = $(plant_image_div).children('.frame');
+            var link = $(plant_image_div).children('a');
+            var href = $(link).attr('href');
+            var title = $(link).attr('title');
+            $(frame).click(function() {
+                // Open the image.
+                Shadowbox.open({
+                    content: href,
+                    player: 'img',
+                    title: title,
+                    options: {
+                        onOpen: photo_helper.prepare_to_enlarge,
+                        onFinish: photo_helper.process_credit
+                    }
+                });
+            });
         });
     };
 
-    return module_function;
-});
+    genus.init = function() {
+        $(document).ready(function() {
+            _setup_page();
+        });
+    };
 
+    return genus;
+});
