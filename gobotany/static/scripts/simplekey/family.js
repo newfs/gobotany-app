d1fc8b4aa47955da761db7e61f933d80e7ae5cb9
Brandon Rhodes <brandon@rhodesmill.org>
2012-07-14 22:23:11 -0400

    Merged sk/family.js logic into family.js (#166)

--- gobotany/static/scripts/simplekey/family.js
+++ gobotany/static/scripts/simplekey/family.js
@@ -1,20 +1,48 @@
 define([
     'bridge/jquery',
-    'gobotany/sk/family',
+    'gobotany/sk/PhotoHelper',
     'util/sidebar',
     'util/activate_search_suggest',
     'util/shadowbox_init',
     'simplekey/glossarize'
-], function($, family, sidebar, activate_search_suggest, shadowbox_init,
-    glossarize) {
+], function($, PhotoHelper, sidebar, activate_search_suggest,
+            shadowbox_init, glossarize) {
 
-    var module_function = function(args) {
-        $(document).ready(function() {
-            glossarize($('.description'));
-            sidebar.setup();
-            family.init(args.family_slug);
+    var family = {};
+
+    var _setup_page = function(args) {
+        glossarize($('.description'));
+        sidebar.setup();
+
+        var photo_helper = PhotoHelper();
+
+        // Wire up each image link to a Shadowbox popup handler.
+        var $images = $('.pics .plant');
+        $images.each(function(i, plant_image_div) {
+            var frame = $(plant_image_div).children('.frame');
+            var link = $(plant_image_div).children('a');
+            var href = $(link).attr('href');
+            var title = $(link).attr('title');
+            $(frame).click(function() {
+                // Open the image.
+                Shadowbox.open({
+                    content: href,
+                    player: 'img',
+                    title: title,
+                    options: {
+                        onOpen: photo_helper.prepare_to_enlarge,
+                        onFinish: photo_helper.process_credit
+                    }
+                });
+            });
         });
     };
 
-    return module_function;
+    family.init = function(args) {
+        $(document).ready(function() {
+            _setup_page(args);
+        });
+    };
+
+    return family;
 });
