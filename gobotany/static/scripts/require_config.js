61a74b58fc4d8a91649619156c294a6488633522
JR Rickerson <jrrickerson@gmail.com>
2012-07-20 15:29:04 -0400

    Use requirejs loader for local development.

---
+++ gobotany/static/scripts/require_config.js
@@ -0,0 +1,11 @@
+requirejs.config({
+    baseUrl: '/static/scripts',
+    paths: {
+        'jquery': 'jquery',
+        'tools': 'tools',
+        'simplekey': 'simplekey',
+        'bridge': 'bridge',
+        'util': 'util'
+    },
+
+});
