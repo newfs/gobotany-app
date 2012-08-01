e229082e437289a48e26a889faf49cdb99b74069
Brandon Rhodes <brandon@rhodesmill.org>
2012-08-01 13:44:34 -0400

    Very simple dkey Couplet and Lead model #190

---
+++ gobotany/dkey/models.py
@@ -0,0 +1,17 @@
+"""Data model for the dichotomous key."""
+
+from django.db import models
+
+class Couplet(models.Model):
+    title = models.TextField(db_index=True)
+    chapter_name = models.TextField()
+    rank = models.TextField()
+    text = models.TextField()
+
+class Lead(models.Model):
+    letter = models.TextField()
+    text = models.TextField()
+    from_couplet = models.ForeignKey(
+        'Couplet', db_index=True, related_name='leads_from')
+    to_couplet = models.ForeignKey(
+        'Couplet', db_index=True, related_name='leads_to')
