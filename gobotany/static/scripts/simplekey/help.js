c1c7566470495b2793a792c5af2395edcb447eb1
John G <jnga@gmx.com>
2012-08-08 11:01:12 -0400

    Obfuscate, link, format email addresses on About, Teaching. (#204)

--- gobotany/static/scripts/simplekey/help.js
+++ gobotany/static/scripts/simplekey/help.js
@@ -2,8 +2,12 @@ require([
     'util/activate_video_links',
     'bridge/shadowbox',
     'util/shadowbox_init',
-    'util/sidebar'
-], function(video_links, Shadowbox, shadowbox_init, sidebar) {
-    sidebar.setup()
-});
+    'util/sidebar',
+    'util/mailto'
+], function(video_links, Shadowbox, shadowbox_init, sidebar, mailto) {
+    sidebar.setup();
 
+    $(document).ready(function () {
+        mailto.make_link('.email');
+    });
+});
