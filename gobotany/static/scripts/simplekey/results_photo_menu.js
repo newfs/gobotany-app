48a88b5f1a09981a3ad71d66ada70beed8761f5b
John G <jnga@gmx.com>
2012-08-10 11:38:07 -0400

    Omit comparison images from Level 3 page photos menu.
    The comparison images are for the species page only. Our convention is to only
    include image types in the Level 3 page photos menu if there are images in that
    category for most of the plants. There are only a few comparison images. (#203)

--- gobotany/static/scripts/simplekey/results_photo_menu.js
+++ gobotany/static/scripts/simplekey/results_photo_menu.js
@@ -3,64 +3,69 @@ define([], function() {
     var results_photo_menu = {
         "woody-angiosperms": {
             "default": "plant form",
-            "omit": ["additional features", "stems"]
+            "omit": ["additional features", "comparison", "stems"]
         },
         "woody-gymnosperms": {
             "default": "plant form",
-            "omit": []
+            "omit": ["comparison"]
         },
         "non-thalloid-aquatic": {
             "default": "plant form",
-            "omit": ["additional features", "detail of leaf and/or divisions",
+            "omit": ["additional features", "comparison",
+                     "detail of leaf and/or divisions",
                      "flowers and fruits", "leaf", "leaves and auricles",
                      "ligules", "shoots", "sori", "special features",
-                     "spikelets", "spore cones", "spores", "stems and sheaths",
-                     "vegetative leaves"]
+                     "spikelets", "spore cones", "spores",
+                     "stems and sheaths", "vegetative leaves"]
         },
         "thalloid-aquatic": {
             "default": "plant form",
-            "omit": []
+            "omit": ["comparison"]
         },
         "carex": {
             "default": "plant form",
-            "omit": []
+            "omit": ["comparison"]
         },
         "poaceae": {
             "default": "plant form",
-            "omit": ["flowers and fruits", "stems"]
+            "omit": ["comparison", "flowers and fruits", "stems"]
         },
         "remaining-graminoids": {
             "default": "plant form",
-            "omit": ["leaves", "special features", "stems"]
+            "omit": ["comparison", "leaves", "special features", "stems"]
         },
         "orchid-monocots": {
             "default": "flowers",
-            "omit": []
+            "omit": ["comparison"]
         },
         "non-orchid-monocots": {
             "default": "flowers",
-            "omit": ["flowers and fruits", "special features", "stems"]
+            "omit": ["comparison", "flowers and fruits", "special features",
+                     "stems"]
         },
         "monilophytes": {
             "default": "plant form",
-            "omit": ["flowers and fruits", "inflorescences", "stems"]
+            "omit": ["comparison", "flowers and fruits", "inflorescences",
+            "stems"]
         },
         "lycophytes": {
             "default": "plant form",
-            "omit": ["flowers and fruits", "inflorescences", "leaves", "stems"]
+            "omit": ["comparison", "flowers and fruits", "inflorescences",
+                     "leaves", "stems"]
         },
         "equisetaceae": {
             "default": "plant form",
-            "omit": []
+            "omit": ["comparison"]
         },
         "composites": {
             "default": "flowers",
-            "omit": []
+            "omit": ["comparison"]
         },
         "remaining-non-monocots": {
             "default": "flowers",
-            "omit": ["additional features", "bark", "flowers and fruits",
-                     "inflorescences", "special features", "winter buds"]
+            "omit": ["additional features", "bark", "comparison",
+                     "flowers and fruits", "inflorescences",
+                     "special features", "winter buds"]
         }
     };
     return results_photo_menu;
