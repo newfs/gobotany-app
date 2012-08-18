c87cb086351c93d676a64ceb96fc3a9e2605f1ef
John G <jnga@gmx.com>
2012-08-17 21:58:45 -0400

    New CSS/templates: bring over Video Help Topics page.
    Also clean up HTML and template a bit. (#215)

--- gobotany/site/urls.py
+++ gobotany/site/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns(
     '',
     url(r'^about/', views.about_view, name='site-about'),
     url(r'^start/', views.getting_started_view, name='site-getting-started'),
+    url(r'^video/', views.video_view, name='site-video'),
     url(r'^contributors/', views.contributors_view, name='site-contributors'),
     )
--- gobotany/site/views.py
+++ gobotany/site/views.py
@@ -7,6 +7,8 @@ from django.views.decorators.vary import vary_on_headers
 from gobotany.core.models import Video
 from gobotany.core.partner import which_partner
 
+from gobotany.simplekey.groups_order import ordered_pilegroups, ordered_piles
+
 def per_partner_template(request, template_path):
     partner = which_partner(request)
     return '{0}/{1}'.format(partner.short_name, template_path)
@@ -26,6 +28,35 @@ def getting_started_view(request):
         per_partner_template(request, 'getting_started.html'), {
             'getting_started_youtube_id': youtube_id,
             }, context_instance=RequestContext(request))
+
+def _get_video_dict(title, video):
+    youtube_id = ''
+    if video:
+        youtube_id = video.youtube_id
+    return {
+        'title': title,
+        'youtube_id': youtube_id
+    }
+
+def video_view(request):
+    # The Getting Started video is first, followed by videos for the pile
+    # groups and piles in the order that they are presented in the stepwise
+    # pages at the beginning of plant identification.
+    videos = []
+    getting_started_video = Video.objects.get(title='Getting Started')
+    if getting_started_video:
+        videos.append({'title': getting_started_video.title,
+                       'youtube_id': getting_started_video.youtube_id});
+
+    for pilegroup in ordered_pilegroups():
+        videos.append(_get_video_dict(pilegroup.name, pilegroup.video))
+        for pile in ordered_piles(pilegroup):
+            videos.append(_get_video_dict(pile.name, pile.video))
+
+    return render_to_response('gobotany/video.html', {
+           'videos': videos,
+           }, context_instance=RequestContext(request))
+
 
 def contributors_view(request):
     return render_to_response('gobotany/contributors.html', {
