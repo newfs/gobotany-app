3a77c455917d00ed60d372c95de05b12dbc884cc
JR Rickerson <jrrickerson@gmail.com>
2012-08-28 15:50:51 -0400

    Include urls for authentication apps.

--- gobotany/plantshare/urls.py
+++ gobotany/plantshare/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls.defaults import patterns, url, include
 
 from gobotany.plantshare import views
 
@@ -9,4 +9,9 @@ urlpatterns = patterns(
     url(r'^$', views.plantshare_view, name='ps-main'),
 
     url(r'^join/$', views.join_view, name='ps-join'),
+
+    # Facebook login
+    url(r'^facebook_connect/', include('facebook_connect.urls')),
+    # Normal registration login
+    url(r'^accounts/', include('registration.backends.default.urls')),
     )
