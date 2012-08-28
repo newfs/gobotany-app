777ea08f0d40c2b70fc23655672fb774d414f219
JR Rickerson <jrrickerson@gmail.com>
2012-08-28 13:25:50 -0400

    Create a basic User profile for registration.

--- gobotany/plantshare/models.py
+++ gobotany/plantshare/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+
+    display_name = models.CharField(max_length=60, unique=True, blank=True)
+    zipcode = models.CharField(max_length=5, blank=True)
+    security_question = models.CharField(max_length=100, blank=True)
+    security_answer = models.CharField(max_length=100, blank=True)
