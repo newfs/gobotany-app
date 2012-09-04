1ceb4c6644a7bd195c8ee54b4d53df8f60978377
John G <jnga@gmx.com>
2012-09-04 17:23:38 -0400

    PlantShare: link to working registration form.
    Title is now Sign Up, like Twitter or FB rather than Join, Register. (#223)

--- gobotany/plantshare/urls.py
+++ gobotany/plantshare/urls.py
@@ -8,8 +8,6 @@ urlpatterns = patterns(
     # PlantShare main page
     url(r'^$', views.plantshare_view, name='ps-main'),
 
-    url(r'^join/$', views.join_view, name='ps-join'),
-
     # Facebook login
     url(r'^facebook_connect/', include('facebook_connect.urls')),
     # Normal registration login
