e7ab3eb512c2bc2a84774cb7e5624a87c2bfb00e
Brandon Rhodes <brandon@rhodesmill.org>
2012-09-08 21:31:26 -0400

    The nightly script can now be tested "read-only"
    With this improvement, a developer can run the nightly.sh script on
    their own Heroku account, and by setting "READ_ONLY" can avoid doing
    real writes to S3 thumbnails and files, since only Sid should be making
    real changes to the S3 filesystem.

--- bin/nightly.sh
+++ bin/nightly.sh
@@ -2,6 +2,13 @@
 
 set -e
 
-$(dirname "$0")/s3imagescan.sh
-$(dirname "$0")/s3thumbnail.sh
+if [ -z "$READ_ONLY" ]
+then
+    $(dirname "$0")/s3imagescan.sh
+    $(dirname "$0")/s3thumbnail.sh
+else
+    echo
+    echo '$READ_ONLY is set - skipping S3 scanning and thumbnailing'
+    echo
+fi
 $(dirname "$0")/s3imageload.sh
