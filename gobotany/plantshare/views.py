ed061e8b9c4e2bd52ee7b5d7f1114d893080206b
John G <jnga@gmx.com>
2012-09-10 13:43:58 -0400

    PlantShare: remove obsolete view. (#223)

--- gobotany/plantshare/views.py
+++ gobotany/plantshare/views.py
@@ -4,7 +4,3 @@ from django.template import RequestContext
 def plantshare_view(request):
     return render_to_response('plantshare.html', {
            }, context_instance=RequestContext(request))
-
-def join_view(request):
-    return render_to_response('join.html', {
-           }, context_instance=RequestContext(request))
