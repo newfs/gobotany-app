4d9875b9e558f30177b84c2831cbe940afc3c1f5
John G <jnga@gmx.com>
2012-09-10 15:35:11 -0400

    Add some more basic functional tests for PlantShare. (#223)

--- gobotany/plantshare/tests.py
+++ gobotany/plantshare/tests.py
@@ -1,3 +1,5 @@
+"""Basic PlantShare functional tests that do not require a browser."""
+
 from gobotany.libtest import FunctionalCase
 
 class PlantShareTests(FunctionalCase):
@@ -6,16 +8,38 @@ class PlantShareTests(FunctionalCase):
         PLANTSHARE_BASE = '/ps'
         self.get(PLANTSHARE_BASE + url_path)
 
-    def test_main_plantshare_page(self):
-        self._get_plantshare('/')
+    # Main page
+
+    MAIN_URL = '/'
+
+    def test_main_page_title(self):
+        self._get_plantshare(self.MAIN_URL)
         title = self.css1('title').text
         self.assertEqual(title, 'PlantShare: Go Botany')
+
+    def test_main_page_main_heading(self):
+        self._get_plantshare(self.MAIN_URL)
         h1 = self.css1('h1').text
         self.assertEqual(h1, 'PlantShare')
 
-    def test_signup_page(self):
-        self._get_plantshare('/accounts/register/')
+    def test_main_page_has_signup_call_to_action(self):
+        self._get_plantshare(self.MAIN_URL)
+        self.assertTrue(self.css1('.sign-up'))
+
+    def test_main_page_has_login_form(self):
+        self._get_plantshare(self.MAIN_URL)
+        self.assertTrue(self.css1('#login.box'))
+
+    # Sign Up form page
+
+    SIGNUP_FORM_URL = '/accounts/register/'
+
+    def test_signup_page_title(self):
+        self._get_plantshare(self.SIGNUP_FORM_URL)
         title = self.css1('title').text
         self.assertEqual(title, 'Sign Up for PlantShare: Go Botany')
+
+    def test_signup_page_main_heading(self):
+        self._get_plantshare(self.SIGNUP_FORM_URL)
         h1 = self.css1('h1').text
         self.assertEqual(h1, 'Sign Up for PlantShare')
