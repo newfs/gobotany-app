7a98467018432e9ee38f89feeab63e086655b123
John G <jnga@gmx.com>
2012-10-02 12:48:33 -0400

    Correct the page title of the Full Key level 1 page.

--- gobotany/simplekey/tests.py
+++ gobotany/simplekey/tests.py
@@ -6,6 +6,34 @@ from gobotany.libtest import FunctionalCase
 from selenium.common.exceptions import NoSuchElementException
 
 
+class SimpleKeyTests(FunctionalCase):
+
+    def test_simple_first_level_page_title(self):
+        self.get('/simple/')
+        title = self.css1('title').text
+        self.assertEqual(title,
+            'Simple Key for Plant Identification: Go Botany')
+
+    def test_simple_first_level_page_main_heading(self):
+        self.get('/simple/')
+        heading = self.css1('#main h2').text
+        self.assertEqual(heading, 'Simple Key')
+
+
+class FullKeyTests(FunctionalCase):
+
+    def test_full_first_level_page_title(self):
+        self.get('/full/')
+        title = self.css1('title').text
+        self.assertEqual(title,
+            'Full Key for Plant Identification: Go Botany')
+
+    def test_full_first_level_page_main_heading(self):
+        self.get('/full/')
+        heading = self.css1('#main h2').text
+        self.assertEqual(heading, 'Full Key')
+
+
 class FamilyTests(FunctionalCase):
 
     def test_family_page(self):
