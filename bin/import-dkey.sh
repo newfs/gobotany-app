76e85c5121c6bbba7658627d635a0d94451e42ba
Brandon Rhodes <brandon@rhodesmill.org>
2012-10-02 13:20:22 -0400

    Stop s3cmd progress bar spamming heroku logs #228

--- bin/import-dkey.sh
+++ bin/import-dkey.sh
@@ -4,6 +4,7 @@ set -e -v
 cd $(dirname "$0")/..
 
 bin/s3-init.sh
-s3cmd get --skip-existing s3://newfs/data-dkey/110330_fone_test_05.xml
+s3cmd get --skip-existing --no-progress \
+    s3://newfs/data-dkey/110330_fone_test_05.xml
 python -m gobotany.dkey.xml_import
 python -m gobotany.dkey.sync
