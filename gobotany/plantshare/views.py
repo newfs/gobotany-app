cadca7cc1dc3b1b75fbc1613b999cafcee828b67
John G <jnga@gmx.com>
2012-10-03 11:20:58 -0400

    Reorganize and improve the views for adding a sighting.
    Go for a more RESTful approach where a new sighting is POSTed
    to the collections URL rather than its form URL. (#235)

--- gobotany/plantshare/views.py
+++ gobotany/plantshare/views.py
@@ -1,38 +1,58 @@
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
 
 from gobotany.plantshare.forms import NewSightingForm
 
+def _new_sighting_form_page(request, form):
+    """Give a new-sighting form, either blank or with as-yet-invalid data."""
+    return render(request, 'new_sighting.html', {
+               'form': form,
+           }, context_instance=RequestContext(request))
+
+# Views
+
 def plantshare_view(request):
+    """View for the main PlantShare page."""
     return render_to_response('plantshare.html', {
            }, context_instance=RequestContext(request))
 
 def sightings_view(request):
-    return render_to_response('sightings.html', {
-           }, context_instance=RequestContext(request))
-
-@login_required
-def new_sighting_view(request):
+    """View for the sightings collection, and handling new sightings."""
     if request.method == 'POST':
+        # Handle posting a new sighting to the sightings collection.
+        # TODO: require login for just this HTTP verb.
         form = NewSightingForm(request.POST)
         if form.is_valid():
             # TODO: process the data in form.cleaned_data
-            return HttpResponseRedirect(reverse('ps-new-sighting-done'))
+            return HttpResponseRedirect(reverse('ps-new-sighting-done')) # ?
+        else:
+            # Present the new-sighting form again for input correction.
+            return _new_sighting_form_page(request, form)
+    elif request.method == 'GET':
+        # Return a representation of the collection of sightings.
+        return render_to_response('sightings.html', {
+               }, context_instance=RequestContext(request))
     else:
-        form = NewSightingForm()
-    return render(request, 'new_sighting.html', {
-        'form': form,
-    })
+        # For an unsupported HTTP method, return a Bad Request response.
+        return HttpResponse(status=400)
+
+@login_required
+def new_sighting_view(request):
+    """View for a blank form for posting a new sighting."""
+    form = NewSightingForm()
+    return _new_sighting_form_page(request, form)
 
 @login_required
 def new_sighting_done_view(request):
+    """View for a confirmation page upon posting a new sighting."""
     return render_to_response('new_sighting_done.html', {
            }, context_instance=RequestContext(request))
 
 @login_required
 def profile_view(request):
+    """View for the logged-in user's profile."""
     return render_to_response('profile.html', {
            }, context_instance=RequestContext(request))
