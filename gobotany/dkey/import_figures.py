6ffd40ef45e2c96259686504f5e3ae384369c359
Brandon Rhodes <brandon@rhodesmill.org>
2012-11-03 18:32:59 -0400

    Module import_figures saves figure images from zip

--- gobotany/dkey/import_figures.py
+++ gobotany/dkey/import_figures.py
@@ -1,7 +1,21 @@
 """Command-line utility to import figure images from zip file."""
 
+import Image
 import argparse
+import os
+import re
 import zipfile
+from StringIO import StringIO
+
+def figure_number(name):
+    if '/maps/' in name:
+        return None
+    if '/prog-keep/' in name:
+        return None
+    numbers = re.findall('\d+(?=[^/]*$)', name)
+    if not numbers:
+        return None
+    return int(numbers[-1])
 
 def main():
     parser = argparse.ArgumentParser(
@@ -10,6 +24,20 @@ def main():
     args = parser.parse_args()
 
     z = zipfile.ZipFile(args.path)
+    names = set(name for name in z.namelist()
+                if figure_number(name) is not None)
+
+    if not os.path.isdir('dkey-figures'):
+        os.mkdir('dkey-figures')
+
+    for name in names:
+        z.open(name)
+        sio = StringIO(z.open(name).read())
+        image = Image.open(sio)
+        number = figure_number(name)
+        path = 'dkey-figures/figure-{}.png'.format(number)
+        image.save(path)
+
 
 if __name__ == '__main__':
     main()
