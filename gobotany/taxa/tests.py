9b316d1eb43024309130b81b4daa253e2856c129
John G <jnga@gmx.com>
2012-11-05 14:53:24 -0500

    Finish writing an unfinished test and cut tests that moved. (#291)

--- gobotany/taxa/tests.py
+++ gobotany/taxa/tests.py
@@ -132,8 +132,9 @@ class SpeciesPageTests(FunctionalCase):
     def test_non_simple_key_species_page_has_note_about_data(self):
         # Temporarily, non-Simple-Key pages show a data disclaimer.
         self.get('/species/adiantum/aleuticum/')
-        self.assertTrue(self.css1('.content .note'))
-
+        note = self.css('.content .note')[0].text
+        self.assertEqual(note, ('Data collection in progress. Complete data '
+                                'are coming soon.'))
 
 class LookalikesTests(FunctionalCase):
 
