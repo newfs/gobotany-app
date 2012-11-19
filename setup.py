11c2b1ae97fd4cad673f941598df02ee5cbe7604
Brandon Rhodes <brandon@rhodesmill.org>
2012-11-18 20:21:53 -0500

    Moved requirements into setup.py for tox #238
    I will ask the other team members about whether we should use tox.
    Either way, making our dependencies part of setup.py is more standard
    than Kenneth's instructions on Heroku that we put them in a pip-only
    requirements.txt file.

--- setup.py
+++ setup.py
@@ -1,3 +1,72 @@
-# Enough information for developers to "pip install -e" the package.
 from distutils.core import setup
-setup(name='gobotany', packages=['gobotany'])
+from setuptools import find_packages
+
+requirements = [
+    'django==1.4',
+
+    'PIL',
+    'South==0.7.6',
+    'cssselect',
+    'django-debug-toolbar',
+    'django-haystack==1.2.7',
+    'django-imagekit==2.0.2',
+    'django-piston==0.2.2',
+    'django-tinymce',
+    'lxml',
+    'psycopg2>=2.3',
+    'python-memcached',
+    'xlrd',
+
+    # The way we use pysolr introduces an extra dependency
+
+    'pysolr==2.0.15',
+    'BeautifulSoup==3.2.1',
+
+    # Memcached on Heroku
+
+    'django-pylibmc-sasl==0.2.4',
+    'pylibmc==1.2.2',
+
+    # Login and registration
+
+    'django-facebook-connect',
+    'django-recaptcha==0.0.4',
+    'django-registration==0.8',
+
+    # For storing images on S3.
+
+    'boto==2.2.2',
+    'django-storages==1.1.4',
+    'requests',
+
+    # Heroku and deployment
+
+    'gunicorn',
+    'newrelic',
+    's3cmd',
+    ]
+
+dependency_links = [
+    'git+https://github.com/noamsu/django-facebook-connect'
+    + '#egg=django-facebook-connect',
+    ]
+
+packages = find_packages()
+package_data = {package: ['templates/*.*', 'templates/*/*.*']
+                for package in packages}
+package_data['gobotany'].append('static/*.*')
+package_data['gobotany'].append('static/*/*.*')
+package_data['gobotany'].append('static/*/*/*.*')
+package_data['gobotany'].append('static/*/*/*/*.*')
+package_data['gobotany.api'].append('testdata/*.*')
+package_data['gobotany.core'].append('image_categories.csv')
+package_data['gobotany.core'].append('testdata/*.*')
+
+setup(
+    name='gobotany',
+    py_modules=['bulkup'],
+    packages=packages,
+    package_data=package_data,
+    install_requires=requirements,
+    dependency_links=dependency_links,
+    )
