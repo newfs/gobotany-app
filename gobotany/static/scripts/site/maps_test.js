b734c68445627d2470c6132302e6a7b4b677d84b
John G <jnga@gmx.com>
2012-11-19 11:39:25 -0500

    Improve maps test page JS and add overlay mask test code. (#292)

--- gobotany/static/scripts/site/maps_test.js
+++ gobotany/static/scripts/site/maps_test.js
@@ -4,30 +4,61 @@ define([
     'mapping/sightings_map'
 ], function ($, google_maps, SightingsMap) {
 
-    $(document).ready(function () {
-        $('div.map[data-type="sightings"]').each(function () {
-            var sightings_map = new SightingsMap(this);
+    var MapsTest = function () {   // Constructor
+
+        // Set up maps when the page has loaded.
+        $(document).ready(function () {
+
+            $('div.map[data-type="sightings"]').each(function () {
+                var sightings_map = new SightingsMap(this);
+                sightings_map.setup();
+                sightings_map.mark_center();
+            });
+
+            // Set up basic sightings map centered on New England.
+            var new_england_map_div = $('#new-england-map');
+            var new_england_sightings_map =
+                new SightingsMap(new_england_map_div);
+            new_england_sightings_map.setup();
+            new_england_sightings_map.mark_center();
+
+            // Set up map for mini Sightings Locator.
+            var mini_sightings_map_div = $('#mini-sightings-map');
+            var mini_sightings_map = new SightingsMap(mini_sightings_map_div);
+            mini_sightings_map.setup();
+            mini_sightings_map.show_plant('Acer saccharum');
+
+            // Set up map for Sightings Locator.
+            var sightings_map_div = $('#sightings-map');
+            var sightings_map = new SightingsMap(sightings_map_div);
             sightings_map.setup();
-            sightings_map.mark_center();
+            sightings_map.show_plant('Nymphaea odorata');
         });
+    };
+        
+    // Functions to test and repair a bug with an overlay mask.
 
-        // Set up basic sightings map centered on New England.
-        var new_england_map_div = $('#new-england-map');
-        var new_england_sightings_map = new SightingsMap(new_england_map_div);
-        new_england_sightings_map.setup();
-        new_england_sightings_map.mark_center();
+    MapsTest.prototype.expose = function () {
+        $(this).expose();
+    };
 
-        // Set up map for mini Sightings Locator.
-        var mini_sightings_map_div = $('#mini-sightings-map');
-        var mini_sightings_map = new SightingsMap(mini_sightings_map_div);
-        mini_sightings_map.setup();
-        mini_sightings_map.show_plant('Acer saccharum');
+    MapsTest.prototype.show_overlay = function () {
+        $('#overlay-test').overlay({
+            mask: {
+                color: '#ccc',
+                loadSpeed: 500,
+                opacity: 0.5,
+                top: 0
+            },
+            closeOnClick: true,
+            load: true
+        }).click(function (event) {
+            $('#overlay-test').data('overlay').close();
+        });
+        $('#exposeMask').css('position', 'fixed');
+    };
 
-        // Set up map for Sightings Locator.
-        var sightings_map_div = $('#sightings-map');
-        var sightings_map = new SightingsMap(sightings_map_div);
-        sightings_map.setup();
-        sightings_map.show_plant('Nymphaea odorata');
-    });
+    // Return the constructor function.
+    return MapsTest;
 });
 
