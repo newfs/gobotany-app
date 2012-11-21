456f6645239ed80f9500e373ab0fae12c8e0f2b1
John G <jnga@gmx.com>
2012-11-20 20:36:45 -0500

    Add plant name input box to Sightings Locator. (#292)

--- gobotany/static/scripts/plantshare/plantshare.js
+++ gobotany/static/scripts/plantshare/plantshare.js
@@ -8,11 +8,11 @@ define([
 
         // Set up the map and form for the mini Sightings Locator.
 
-        var mini_sightings_map_div = $('#mini-locator .map');
+        var mini_sightings_map_div = $('#sightings-locator.mini .map');
         var mini_sightings_map = new SightingsMap(mini_sightings_map_div);
         mini_sightings_map.setup();
 
-        $('#mini-locator form').submit(function (e) {
+        $('#sightings-locator.mini form').submit(function (e) {
             e.preventDefault();   // prevent form submit
 
             console.log('TODO: call the server for sightings data');
