dfd18018af7ea1ee0b7e3b970dc6df9f86db0547
John G <jnga@gmx.com>
2012-11-20 19:52:46 -0500

    Link the mini sightings locator to the Sightings page. (#292)

--- gobotany/static/scripts/plantshare/plantshare.js
+++ gobotany/static/scripts/plantshare/plantshare.js
@@ -8,11 +8,11 @@ define([
 
         // Set up the map and form for the mini Sightings Locator.
 
-        var mini_sightings_map_div = $('#mini-sightings-map');
+        var mini_sightings_map_div = $('#mini-locator .map');
         var mini_sightings_map = new SightingsMap(mini_sightings_map_div);
         mini_sightings_map.setup();
 
-        $('#sightings-locator form').submit(function (e) {
+        $('#mini-locator form').submit(function (e) {
             e.preventDefault();   // prevent form submit
 
             console.log('TODO: call the server for sightings data');
