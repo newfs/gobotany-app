6959c227e894d18fb7b1eb05ba3cfbc9e14409e6
John G <jnga@gmx.com>
2012-11-23 07:33:38 -0500

    Adjust Help section pages and CSS. (#297)

--- gobotany/site/urls.py
+++ gobotany/site/urls.py
@@ -18,13 +18,13 @@ urlpatterns = patterns(
 
     # Help section
     url(r'^help/$', views.help_view, name='site-help'),
-    url(r'^about/$', views.about_view, name='site-about'),
     url(r'^start/$', views.getting_started_view, name='site-getting-started'),
+    url(r'^video/$', views.video_view, name='site-video'),
     url(r'^map/$', views.advanced_map_view, name='site-advanced-map'),
     url(r'^glossary/(?P<letter>[1a-z])/$', views.glossary_view,
         name='site-glossary'),
     url(r'^glossary/$', views.glossary_main_view, name='site-glossary-main'),
-    url(r'^video/$', views.video_view, name='site-video'),
+    url(r'^about/$', views.about_view, name='site-about'),
     url(r'^contributors/$', views.contributors_view,
         name='site-contributors'),
 
