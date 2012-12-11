77dc504e196f6103aa1e591e474c4fc3efac2e75
John G <jnga@gmx.com>
2012-12-11 08:24:17 -0500

    Allow passing variables to instantiate sightings maps.
    Like the location maps, the sightings maps now use an inclusion tag,
    which allows template variables to be passed in. This improves
    flexibility and makes the mapping API tags more uniform. (#292)

--- gobotany/mapping/templatetags/mapping_tags.py
+++ gobotany/mapping/templatetags/mapping_tags.py
@@ -18,3 +18,23 @@ def location_map(**kwargs):
         'zoom': zoom,
         'id': id
     }
+
+@register.inclusion_tag('_sightings_map.html')
+def sightings_map(**kwargs):
+    latitude = kwargs['latitude']
+    longitude = kwargs['longitude']
+    center_title = kwargs['center_title']
+    height = kwargs['height']
+    width = kwargs['width']
+    zoom = kwargs['zoom']
+    id = kwargs['id']
+    return {
+        'latitude': latitude,
+        'longitude': longitude,
+        'center_title': center_title,
+        'height': height,
+        'width': width,
+        'zoom': zoom,
+        'id': id
+    }
+
