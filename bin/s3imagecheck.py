24062cf299950afe77bd2e56a99cdb336716a64c
Brandon Rhodes <brandon@rhodesmill.org>
2012-12-11 21:44:11 -0500

    Refactor s3imagecheck #318
    In preparation for teaching s3imagecheck.sh to scan our thumbnails to
    see whether they are present, it needed refactoring so that its
    routines are shorter and the logic occurs in smaller steps.

--- bin/s3imagecheck.py
+++ bin/s3imagecheck.py
@@ -9,91 +9,132 @@ allow themselves to be downloaded.  An attempt is made to fix images
 with bad permissions.
 
 """
-import gzip
-import os
 import re
 import socket
 import time
-from StringIO import StringIO
 
 import boto
 import requests
 
+DEBUG = False
 CACHE_CONTROL = 'max-age=28800, public'
 THUMBNAIL_DIRS = '160x149', '239x239', '1000s1000'
 url_pattern = re.compile(r'/taxon-images/[A-Z]')
 
-def generate_paths(bucket):
-    ls_taxon_images_gz = bucket.get_key('ls-taxon-images.gz').read()
-    g = gzip.GzipFile(fileobj=StringIO(ls_taxon_images_gz))
-    for line in g:
-        fields = line.split()
-        url = fields[3]
-        if not url_pattern.search(url):
-            continue
-        yield url
-        for dirname in THUMBNAIL_DIRS:
-            yield url.replace('/taxon-images', '/taxon-images-' + dirname)
-
 def main():
-    conn = boto.connect_s3()
-    bucket = conn.get_bucket('newfs')
+    operator = Operator('newfs')
+    family_names = operator.list_families()
+    for family_name in family_names:
+        if DEBUG and family_name != 'Dryopteridaceae':
+            continue  # skip all but one family to speed debugging
+        check_family(operator, family_name)
+    operator.final_report()
 
-    directory_count = 0
-    image_count = 0
-    error_count = 0
-    bad_header_count = 0
+def check_family(operator, family_name):
+    """Check all the images and thumbnails for a family of plants.
 
-    hostname = 'newfs.s3.amazonaws.com'
-    ip = socket.gethostbyname(hostname)
-    session = requests.Session()
+    Families are reasonable-sized chunks of our image collection, so
+    this main image-checking and thumbnail-checking routine operates on
+    one family each time it is called.
 
-    t0 = time.time()
-    for url in generate_paths(bucket):
-        # if 'Dryopteridaceae' not in url:
-        #     continue  # limit running time, for debugging
-        path = url.replace('s3://newfs', '')
-        url = 'http://{}{}'.format(ip, path)
-        headers = {'Host': hostname}
-        if url.endswith('/'):
-            directory_count += 1
-            expected_code = 403
-        elif url.endswith('.jpg'):
-            image_count += 1
-            expected_code = 200
+    """
+    images = list(operator.list_images(family_name))
+    check_images(operator, images)
+
+def check_images(operator, keys):
+    for key in keys:
+        if key.name.endswith('/'):
+            check_directory(operator, key)
         else:
-            print 'Warning: unrecognized {}'.format(url)
-            continue
+            check_image(operator, key)
 
-        response = session.head(url, headers=headers)
-        if response.status_code != expected_code:
-            print 'Error: {} -> {}'.format(url, response.status_code)
-            error_count += 1
-            key = bucket.get_key(path)
-            if key is None:
-                print '       Resource does not exist'
-            elif response.status_code == 200:
-                print '       Making resource private'
-                key.set_acl('private')
-            elif response.status_code == 403:
-                print '       Making resource public'
-                key.make_public(headers={'cache-control': CACHE_CONTROL})
-            continue
+def check_directory(operator, key):
+    r = operator.head(key.name)
+    if r.status_code != 403:
+        operator.error(key, 'Status code {} != 403 - making private'
+                       .format(r.status_code))
+        key.set_acl('private')
 
-        cc = response.headers.get('cache-control')
-        if not url.endswith('/') and cc != CACHE_CONTROL:
-            print 'Error: {} cache-control = {!r}'.format(url, cc)
-            bad_header_count += 1
-            continue
+def check_image(operator, key):
+    if not key.name.endswith('.jpg'):
+        operator.error(key, 'Unrecognized image extension')
+        return
+    r = operator.head(key.name)
+    if r.status_code != 200:
+        operator.error(key, 'Making image public; status code was {}'
+                       .format(r.status_code))
+        key.make_public(headers={'cache-control': CACHE_CONTROL})
+        return
+    content_type = r.headers.get('content-type')
+    if content_type != 'image/jpeg':
+        operator.error(key, 'Bad image content-type: {}'.format(content_type))
+        # Not sure what to do about this one; shouldn't it get set to
+        # the correct value automatically thanks to the upload tool?
+        return
+    cache_control = r.headers.get('cache-control')
+    if cache_control != CACHE_CONTROL:
+        operator.error(key, 'Fixing bad cache-control value: {}'
+                       .format(cache_control))
+        key.make_public(headers={'cache-control': CACHE_CONTROL})
+        return
 
-    elapsed = time.time() - t0
-    per_second = (directory_count + image_count) / elapsed
+class Operator(object):
+    """Perform checks and run actions on our image database.
 
-    print 'Scanned {} directories'.format(directory_count)
-    print 'Scanned {} images'.format(image_count)
-    print 'Scanned {:.2f} resources per second'.format(per_second)
-    print 'Found {} errors'.format(error_count)
-    print 'Found {} bad headers'.format(bad_header_count)
+    This class is convenient - it is the only thing that the above code
+    needs to pass around to be able to talk to our S3 bucket both over
+    normal HTTP and also through boto - and it is also very efficient,
+    because it holds open HTTP sessions and also caches our S3 hostname
+    to avoid repeated DNS lookups.
+
+    """
+    def __init__(self, bucket_name):
+        boto_connection = boto.connect_s3()
+        self.bucket = boto_connection.get_bucket('newfs')
+
+        self.t0 = time.time()
+        self.directory_count = 0
+        self.image_count = 0
+        self.error_count = 0
+
+        self.hostname = '{}.s3.amazonaws.com'.format(bucket_name)
+        self.cached_ip = socket.gethostbyname(self.hostname)
+        self.session = requests.Session()
+
+    def head(self, path):
+        if not path.startswith('/'):
+            path = '/' + path
+        if path.endswith('/'):
+            self.directory_count += 1
+        else:
+            self.image_count += 1
+        url = 'http://{}{}'.format(self.cached_ip, path)
+        headers = {'Host': self.hostname}
+        response = self.session.head(url, headers=headers)
+        return response
+
+    def list_families(self):
+        seq = self.bucket.list('taxon-images/', '/')
+        return [ key.name.strip('/').split('/')[-1] for key in seq ]
+
+    def list_images(self, family_name):
+        seq = self.bucket.list('taxon-images/{}/'.format(family_name))
+        return seq
+
+    def error(self, key, message):
+        print key.name
+        print ' ', message
+        self.error_count += 1
+
+    def final_report(self):
+        elapsed = time.time() - self.t0
+        per_second = (self.directory_count + self.image_count) / elapsed
+        print
+        print 'Scan took {:.2f} seconds'.format(elapsed)
+        print 'Scanned {:.2f} resources per second'.format(per_second)
+        print 'Scanned {} directories'.format(self.directory_count)
+        print 'Scanned {} images'.format(self.image_count)
+        print 'Found {} errors'.format(self.error_count)
 
 if __name__ == '__main__':
     main()
