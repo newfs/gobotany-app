41df1f28951fa76812cd2b7c34dd7d385aa5dbe3
Brandon Rhodes <brandon@rhodesmill.org>
2012-12-25 11:56:27 -0500

    Add "character" to pile-editing URLs #226
    Before creating a second hierarchy beneath the /edit/cv/ top-level URL,
    we need to qualify the existing pile-specific URLs with a "character"
    string.

--- gobotany/editor/urls.py
+++ gobotany/editor/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = patterns(
     '',
     url(r'^cv/$', views.piles_view),
-    url(r'^cv/([^/]+)/$', views.pile_view),
-    url(r'^cv/([^/]+)/([^/]+)/$', views.edit_pile_character),
+    url(r'^cv/([^/]+)-characters/$', views.pile_view),
+    url(r'^cv/([^/]+)-characters/([^/]+)/$', views.edit_pile_character),
     url(r'^.*', views.e404),  # prevent fall-through to wildcard rewrite URL
     )
