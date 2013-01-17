9410c263ad56956efcf25edc3e2db1bebfc06e81
Brandon Rhodes <brandon@rhodesmill.org>
2013-01-16 21:05:21 -0500

    Create a separate view for editing length values

--- gobotany/editor/views.py
+++ gobotany/editor/views.py
@@ -9,14 +9,17 @@ from operator import attrgetter as pluck
 from gobotany.core import models
 from gobotany.core.partner import which_partner
 
+
 def e404(request):
     raise Http404()
+
 
 @permission_required('botanist')
 def piles_view(request):
     return render_to_response('gobotany/edit_piles.html', {
         'piles' : models.Pile.objects.all(),
         }, context_instance=RequestContext(request))
+
 
 @permission_required('botanist')
 def pile_characters(request, pile_slug):
@@ -26,13 +29,26 @@ def pile_characters(request, pile_slug):
         'pile' : get_object_or_404(models.Pile, slug=pile_slug),
         }, context_instance=RequestContext(request))
 
+
 @permission_required('botanist')
 def edit_pile_character(request, pile_slug, character_slug):
 
-    character = get_object_or_404(models.Character, short_name=character_slug)
-    values = sorted(character.character_values.all(), key=character_value_key)
-
     pile = get_object_or_404(models.Pile, slug=pile_slug)
+    character = get_object_or_404(models.Character, short_name=character_slug)
+    values = list(character.character_values.all())
+
+    if character.value_type == 'LENGTH':
+        return _edit_pile_length_character(request, pile, character, values)
+    else:
+        return _edit_pile_string_character(request, pile, character, values)
+
+
+def _edit_pile_length_character(request, pile, character, values):
+    raise NotImplementedError()
+
+
+def _edit_pile_string_character(request, pile, character, values):
+    values.sort(key=character_value_key)
     taxa = list(pile.species.all())
 
     tcvlist = list(models.TaxonCharacterValue.objects
@@ -110,11 +126,13 @@ def edit_pile_character(request, pile_slug, character_slug):
         'values_json': json.dumps([value.value_str for value in values]),
         }, context_instance=RequestContext(request))
 
+
 @permission_required('botanist')
 def pile_taxa(request, pile_slug):
     return render_to_response('gobotany/pile_taxa.html', {
         'pile' : get_object_or_404(models.Pile, slug=pile_slug),
         }, context_instance=RequestContext(request))
+
 
 @permission_required('botanist')
 def edit_pile_taxon(request, pile_slug, taxon_slug):
@@ -170,6 +188,7 @@ def edit_pile_taxon(request, pile_slug, taxon_slug):
         'pile_characters': annotated_characters(pile_characters),
         'taxon': taxon,
         }, context_instance=RequestContext(request))
+
 
 def character_value_key(cv):
     """Return a sort key that puts 'NA' last."""
