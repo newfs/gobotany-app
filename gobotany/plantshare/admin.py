9ac845290d3bf8815ce0de4a7c756973f09772c9
John G <jnga@gmx.com>
2013-02-04 14:05:18 -0500

    Begin setting up a place to answer questions in the Admin. (#351)

---
+++ gobotany/plantshare/admin.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.contrib import admin
+
+from gobotany.plantshare.models import Question
+
+class QuestionAdminForm(forms.ModelForm):
+    question = forms.CharField(
+        widget=forms.Textarea(attrs={'rows':3, 'cols':80})
+    )
+    answer = forms.CharField(
+        widget=forms.Textarea(attrs={'rows':7, 'cols':80})
+    )
+    class Meta:
+        model = Question
+
+class QuestionAdmin(admin.ModelAdmin):
+    date_hierarchy = 'asked'
+    fields = ('question', 'asked_by', 'answer', 'category', 'answered',
+              'answered_by', 'duplicate_of')
+    form = QuestionAdminForm
+    list_display = ('question', 'answer', 'asked', 'category')
+    ordering = ['answer', 'asked']
+    readonly_fields = ['asked_by']
+    search_fields = ['question', 'answer']
+
+admin.site.register(Question, QuestionAdmin)
