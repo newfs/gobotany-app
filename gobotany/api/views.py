4d5ef52b5ff84d9e9700e36ae94e9ae11256ea53
John G <jnga@gmx.com>
2013-03-15 09:42:24 -0400

    Fix the glossaryblob API call, which was failing with an error.
    The error was: 'GlossaryTerm' object has no attribute 'image_path'

--- gobotany/api/views.py
+++ gobotany/api/views.py
@@ -107,12 +107,11 @@ def glossary_blob(request):
             try:
                 url = gt.image.url
                 prefix = url[:-len(gt.image_path)]
-                break
             except ValueError:  # Image not found in storage.
                 pass
 
     images = { gt.term: prefix + gt.image_path for gt in glossaryterms
-               if gt.image_path is not None and prefix is not None}
+               if gt.image_path is not None and prefix is not None }
 
     return jsonify({'definitions': definitions, 'images': images})
 
