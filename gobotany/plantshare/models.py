2dbc3653902a0379e577058e9934ac2ad71d4a95
John G <jnga@gmx.com>
2013-04-11 14:37:48 -0400

    Remove unique constraint on user display name.
    This was causing the import to crash upon loading fixture data,
    because a blank display name was being created and then another
    could not. Since it seems we may want to allow multiple
    instances of the same display name (say, for users with the
    same first and last names), removed the constraint for now.
    However, uniqueness might be desired in the UI, which perhaps
    could be achieved by pairing a display name with the user
    name in parentheses, or similar. (#448)

--- gobotany/plantshare/models.py
+++ gobotany/plantshare/models.py
@@ -72,7 +72,7 @@ class UserProfile(models.Model):
 
     user = models.OneToOneField(User)
 
-    display_name = models.CharField(max_length=60, unique=True, blank=True)
+    display_name = models.CharField(max_length=60, blank=True)
     zipcode = models.CharField(max_length=5, blank=True)
     security_question = models.CharField(max_length=100, blank=True)
     security_answer = models.CharField(max_length=100, blank=True)
