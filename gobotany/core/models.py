eef8f074c06211fa0519f967b5a34558302f20a8
Alec Mitchell <alecpm@gmail.com>
2013-04-11 08:58:27 -0700

    Update lit source max length to requested value.

--- gobotany/core/models.py
+++ gobotany/core/models.py
@@ -628,7 +628,7 @@ class TaxonCharacterValue(models.Model):
     taxon = models.ForeignKey(Taxon)
     character_value = models.ForeignKey(CharacterValue,
                                         related_name='taxon_character_values')
-    lit_source = models.CharField(max_length=100,
+    lit_source = models.CharField(max_length=250,
                                   null=True, blank=True)
 
     class Meta:
