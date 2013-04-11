7271098737ffb4de13a3a8aaa0930a025a52ff9b
John G <jnga@gmx.com>
2013-04-11 11:55:24 -0400

    Fix bug where Log In box no longer appeared in the correct place. (#446)

--- gobotany/static/scripts/plantshare/registration_complete.js
+++ gobotany/static/scripts/plantshare/registration_complete.js
@@ -5,9 +5,9 @@ define([
 
     $(document).ready(function () {
 
-        // Set a cookie indicating that the user completed the registration
-        // process. This will be used to omit the "Sign Up" call to action
-        // when the user is signed out.
+        // Set a cookie indicating that the user has already completed the
+        // registration process. This will be used to omit the "Sign Up"
+        // call to action when the user is signed out.
         $.cookie('registration_complete', 'True',
                  {expires: (10 * 365), path: '/'});
     });
