886bd2dd114d8b7bc3ae0c91cb6b96daa192adf5
JR Rickerson <jrrickerson@gmail.com>
2013-04-12 16:08:18 -0400

    Enable Add / Remove of Checklist entries.

---
+++ gobotany/static/scripts/plantshare/new_checklist.js
@@ -0,0 +1,17 @@
+define([
+    'bridge/jquery', 
+    'util/shadowbox_init',
+    'util/formset'
+], function ($, Shadowbox, formset) {
+
+    $(document).ready(function() {
+
+        formset.init({
+            'formSelector': '#formset tr',
+            'formTemplateSelector': '#form-template tr',
+            'addLinkSelector': '.add-new-row',
+            'removeLinkSelector': '.close-btn.row-btn'
+        });
+
+    });
+});
