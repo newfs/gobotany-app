6ca6f52133fed616d340ab7c0cb2fe6bf55e8cb7
JR Rickerson <jrrickerson@gmail.com>
2013-04-23 17:56:09 -0400

    Enable notes on Edit Checklist page.

--- gobotany/static/scripts/plantshare/edit_checklist.js
+++ gobotany/static/scripts/plantshare/edit_checklist.js
@@ -4,6 +4,18 @@ define([
     'util/formset'
 ], function ($, Shadowbox, formset) {
 
+    var notes_modal = '' +
+    '<div id="container" class="notes-modal">' +
+		'<h1>Add Notes to Item</h1>' +
+		'<section>' +
+            '<textarea placeholder="Write your notes here"></textarea>' +
+            '<div class="form-actions">' +
+                '<a href="#" class="ps-button save">Save</a>' +
+                '<a href="#" class="clear-btn">Clear</a>' +
+            '</div><!-- /.form-actions -->' +
+		'</section>' +
+	'</div><!--!/#container -->';
+
     $(document).ready(function() {
 
         formset.init({
@@ -12,6 +24,39 @@ define([
             'addLinkSelector': '.add-new-row',
             'removeLinkSelector': '.close-btn.row-btn',
             'canDelete': true
+        });
+
+        // Shadowbox setup for Notes modal popups
+        $(document).on('click', 'a.note-link', function(e) {
+            e.preventDefault();
+            var $this = $(this);
+            Shadowbox.open({
+                content:  notes_modal,
+                player: 'html',
+                title: 'Checklist Notes',
+                width: 550,
+                height: 240,
+                options: {
+                    enableKeys: false,
+                    onFinish: function(item) {
+                        var $textarea = $('#container').find('textarea');
+                        var $field = $this.parents('td.note').find('textarea');
+                        $textarea.val($field.val());
+                        $textarea.attr('rel', $field.attr('id'));
+                    }
+                }
+            });
+        });
+        $(document).on('click', '.notes-modal a.save', function(e) {
+            e.preventDefault();
+            var $textarea = $(this).parents('section').find('textarea');
+            var $field = $('#' + $textarea.attr('rel'));
+            $field.val($textarea.val());
+            Shadowbox.close();
+        });
+        $(document).on('click', '.notes-modal a.clear-btn', function(e) {
+            e.preventDefault();
+            $(this).parents('section').find('textarea').val('');
         });
 
     });
