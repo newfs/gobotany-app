87473a3f8264cee2108dce35f37c63878eb94cf1
John G <jnga@gmx.com>
2013-04-23 08:47:09 -0400

    Show a message indicating filter state on the Manage Sightings page. (#440)

--- gobotany/static/scripts/plantshare/manage_sightings.js
+++ gobotany/static/scripts/plantshare/manage_sightings.js
@@ -7,7 +7,20 @@ define([
         // Set up table filtering.
         var options = {
             enableCookies: true,
-            filterDelay: 400
+            filterDelay: 400,
+            filteredRows: function () {
+                // Update the message indicating how many sightings are shown.
+                var num_rows = $('.list tbody tr').length;
+                var num_hidden_rows = $(
+                    '.list tbody tr[style="display: none;"]').length;
+                var num_showing = num_rows - num_hidden_rows;
+                var message = 'are all';
+                if (num_showing < num_rows) {
+                    var verb = (num_showing > 1) ? 'are': 'is';
+                    message = verb + ' ' + num_showing + ' of the';
+                }
+                $('.showing').html(message);
+            }
         };
         $('.content table').tableFilter(options);
     });
