b66fa313432693ae447c25d346225b71c1458579
John G <jnga@gmx.com>
2013-04-25 09:34:01 -0400

    Enable submit button on Find People search boxes. (#441)

--- gobotany/static/scripts/plantshare/find_people.js
+++ gobotany/static/scripts/plantshare/find_people.js
@@ -2,10 +2,6 @@ define([
     'bridge/jquery'
 ], function ($) {
 
-    $(document).ready(function () {
-        var FORM_SELECTOR = '#find-people-form';
-        $(FORM_SELECTOR + ' input[type=button]').click(function () {
-            $(FORM_SELECTOR).submit();
-        });
-    });
+    // TODO: handle View Profile, Invite to Group
+
 });
--- gobotany/static/scripts/plantshare/plantshare.js
+++ gobotany/static/scripts/plantshare/plantshare.js
@@ -3,6 +3,15 @@ define([
     'plantshare/sightings_locator_part'
 ], function ($, SightingsLocatorPart) {
 
+    /*
+    $(document).ready(function () {
+        var FIND_FORM_SELECTOR = '#find-people-form';
+        $(FIND_FORM_SELECTOR + ' input[type=button]').click(function () {
+            $(FIND_FORM_SELECTOR).submit();
+        });
+
+    });*/
+
     $(window).load(function () {   // maps must be created at onload
 
         var sightings_locator_part =
