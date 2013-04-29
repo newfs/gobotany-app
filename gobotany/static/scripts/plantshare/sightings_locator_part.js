7cf983ea462f0b00445e4f9ae06d01b004d1d1c5
John G <jnga@gmx.com>
2013-04-29 13:11:45 -0400

    Maintain the last plant searched in Sightings Locator.
    This is to keep a user's place during navigation to sightings, etc.,
    and back. (#451)

--- gobotany/static/scripts/plantshare/sightings_locator_part.js
+++ gobotany/static/scripts/plantshare/sightings_locator_part.js
@@ -4,13 +4,16 @@
  * The map requires that this be set up at onload, not at jQuery ready. */
 define([
     'bridge/jquery',
+    'bridge/jquery.cookie',
     'plantshare/sightings_map'
-], function ($, SightingsMap) {
+], function ($, x1, SightingsMap) {
 
     function SightingsLocatorPart(locator_element_css) {
         // Constructor
         
         this.$locator_element = $(locator_element_css);
+        this.$plant_name_field = this.$locator_element.find(
+            'input[type="text"]').first();
         this.current_plant_name = '';
     };
 
@@ -21,15 +24,23 @@ define([
         var sightings_map = new SightingsMap(map_div);
         sightings_map.setup();
 
+        // Restore the last plant searched in the Sightings Locator, if any.
+        var plant_name = $.cookie('last_locator_name');
+        if (plant_name !== undefined && plant_name.length > 0) {
+            this.$plant_name_field.val(plant_name);
+            sightings_map.show_plant(plant_name);
+        }
+
         this.$locator_element.submit($.proxy(
             function (e) {
                 e.preventDefault();  // prevent form submit because using AJAX
 
-                var plant_name = this.$locator_element.find(
-                    'input[type="text"]').eq(0).val();
+                var plant_name = this.$plant_name_field.val();
 
                 if (plant_name !== this.current_plant_name) {
                     this.current_plant_name = plant_name;
+                    $.cookie('last_locator_name', this.current_plant_name,
+                             {path: '/'});
                     sightings_map.show_plant(this.current_plant_name);
                 }
 
