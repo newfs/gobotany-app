d4813f4b32d58b1800b4ee633937263b92b49332
John G <jnga@gmx.com>
2013-05-17 09:13:18 -0400

    Use a link for deleting a checklist for now.
    This is to be consistent with sightings, and to only allow
    deleting one checklist at a time. It also allows the checkboxes
    to be hidden, avoiding potential confusion given that the
    feature is checklists, although we may need to revisit this
    when it comes time to add the sharing feature.

--- gobotany/static/scripts/plantshare/checklists.js
+++ gobotany/static/scripts/plantshare/checklists.js
@@ -2,11 +2,19 @@ define([
     'bridge/jquery', 
 ], function ($) {
 
-   $('.delete-btn').click(function(e) {
-       e.preventDefault();
+    $('a.delete').click(function (e) {
+        // Delete a single checklist.
 
-       $('form').attr('action', $(this).attr('href'));
-       $('form').submit();
-   });
+        e.preventDefault();
+
+        var $cells = $(this).parent().siblings();
+        var $checkbox = $cells.first().find('input');
+        // Check the (hidden) checkbox to mark for deletion.
+        $checkbox.prop('checked', true);
+
+        // POST the form to delete it.
+        $('form').attr('action', $(this).attr('href'));
+        $('form').submit();
+    });
 
 })
