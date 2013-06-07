9d78d44813d38a96e5727222feaca0f7150683d7
John G <jnga@gmx.com>
2013-06-07 07:32:16 -0400

    Recognize some typos when matching for rare plant species. (#456)

--- gobotany/plantshare/utils.py
+++ gobotany/plantshare/utils.py
@@ -13,8 +13,27 @@ def restrictions(plant_name):
     # Remove any extra spaces in the plant name.
     plant_name = ' '.join(plant_name.split())
 
+    # Split the words and create regular expressions for typo matching:
+    # - Interior transpositions: pylmouth rose-gentian, plymouth rsoe-gentian
+    # - An extra character: plymoutth rose-gentian
+    # - A missing duplicate character: sabatia kenedyana (missing an 'n')
+    regex_words = []
+    words = plant_name.split()
+    for word in words:
+        if len(word) > 2:
+            interior = ''.join(set(list(word[1:-1]))) # Unique interior chars.
+            regex_word = '%s[%s]{%d,%d}%s' % (
+                word[0],   # First character: an anchor
+                interior,  # Any interior characters, to handle transpositions
+                len(interior),      # Allow typo with an extra character
+                len(interior) + 1,
+                word[-1])  # Last character: another anchor
+        else:
+            regex_word = word
+        regex_words.append(regex_word)
+
     # Allow ignoring hyphens, periods, and other non-word characters.
-    plant_name_regex = plant_name.replace(' ', '\W+')
+    plant_name_regex = '\W+'.join(regex_words)
 
     restrictions = []
 
