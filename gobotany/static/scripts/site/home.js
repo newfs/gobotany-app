3f4c9be09676cc3023f5b4b33e84cc57976afc16
John G <jnga@gmx.com>
2013-07-26 22:29:13 -0400

    Increase home page image fade interval to calm the animation.

--- gobotany/static/scripts/site/home.js
+++ gobotany/static/scripts/site/home.js
@@ -1,6 +1,6 @@
 require([
     'util/fade'
 ], function(fade_next_banner_image) {
-    var FADE_INTERVAL = 6 * 1000; // Includes the fade itself
+    var FADE_INTERVAL = 15 * 1000; // Includes the fade itself
     setInterval(fade_next_banner_image, FADE_INTERVAL);
 });
