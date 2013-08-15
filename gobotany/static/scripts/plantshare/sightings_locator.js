a1a0a072ec0b733c82e3bb3b620b13e843d7b2a0
John G <jnga@gmx.com>
2013-08-15 15:06:58 -0400

    Try fixing maps error by requesting module earlier.
    Errors were intermittent and sometimes not reproducible, but
    have not seen the errors since this change. (#521)

--- gobotany/static/scripts/plantshare/sightings_locator.js
+++ gobotany/static/scripts/plantshare/sightings_locator.js
@@ -1,8 +1,9 @@
 // Sightings Locator page: initialization
 define([
     'bridge/jquery',
+    'mapping/google_maps', // request maps module before creating map control
     'plantshare/sightings_locator_part'
-], function ($, SightingsLocatorPart) {
+], function ($, google_maps, SightingsLocatorPart) {
 
     $(window).load(function () {   // maps must be created at onload
 
