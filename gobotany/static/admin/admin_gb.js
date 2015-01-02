2a52e68773bbd703de818c612e91a7b21a53a820
John G <jnga@users.noreply.github.com>
2015-01-02 15:04:26 -0500

    Admin: Distribution records: repeat name on Save and Add Again. (#598)

--- gobotany/static/admin/admin_gb.js
+++ gobotany/static/admin/admin_gb.js
@@ -18,5 +18,23 @@ django.jQuery(document).ready(function () {
         return false;
     });
 
+    // Distribution records: Customize Save and Add Another by populating
+    // the Scientific Name field with the name from the record just added.
+    var just_added = false,
+        message, scientific_name;
+    if (django.jQuery('#content h1').text() === 'Add Distribution record') {
+        // If a record was just added, use its scientific name again.
+        message = django.jQuery('.messagelist .info').text();
+        if (message.indexOf('was added successfully. ' +
+                'You may add another Distribution record below') > -1) {
+            just_added = true;
+        }
+        if (just_added) {
+            scientific_name = message.split(':')[0].split('"')[1];
+            django.jQuery('#id_scientific_name').val(scientific_name);
+            django.jQuery('#id_state').focus();
+        }
+    }
+
 });
 
