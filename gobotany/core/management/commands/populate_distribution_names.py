19c89f9843d3a4c39bb5ae409859bae9c78dab4e
jnga <jnga@users.noreply.github.com>
2015-05-11 21:08:17 -0400

    Allow splitting distribution records by starting letter. (#600)
    Because of difficulties running the management command to split
    distribution records for the entire set of plants all at once
    in the Heroku environment, it was requested to add a way to
    break this up by the starting letter of the plant name. This
    way the command can be run a bit at a time on the plant records
    so it can complete.

--- gobotany/core/management/commands/populate_distribution_names.py
+++ gobotany/core/management/commands/populate_distribution_names.py
@@ -3,26 +3,41 @@ from django.core.management.base import BaseCommand
 from gobotany.core.models import Distribution
 
 class Command(BaseCommand):
+    """Populate the species_name and subspecific_epithet fields
+    in the Distribution model by splitting the scientific name.
+
+    Accept an optional argument that specifies the starting letters
+    of the species names to process. Example:
+
+    dev/django populate_distribution_names abcd
+    """
+    args = '<abcde>'
     help = ('Splits the scientific_name and populates the species_name and '
         'subspecific_epithet fields')
 
     def handle(self, *args, **options):
-        for record in Distribution.objects.all():
-            parts = record.scientific_name.split(' ')
+        starts_letters = 'abcdefghijklmnopqrstuvwxyz'
+        if len(args) > 0:
+            starts_letters = args[0]
+        for letter in list(starts_letters):
+            for record in Distribution.objects.filter(
+                scientific_name__istartswith=letter):
 
-            if 'x' in parts or 'X' in parts:
-                # handle hybrids
-                species_name = ' '.join(parts[0:3])
-                subspecific_epithet = ' '.join(parts[3:])
-            else:
-                species_name = ' '.join(parts[0:2])
-                subspecific_epithet = ' '.join(parts[2:])
+                parts = record.scientific_name.split(' ')
 
-            record.species_name = species_name
-            record.subspecific_epithet = subspecific_epithet
-            record.save()
+                if 'x' in parts or 'X' in parts:
+                    # handle hybrids
+                    species_name = ' '.join(parts[0:3])
+                    subspecific_epithet = ' '.join(parts[3:])
+                else:
+                    species_name = ' '.join(parts[0:2])
+                    subspecific_epithet = ' '.join(parts[2:])
 
-            message = record.scientific_name + ' --> ' + record.species_name
-            if record.subspecific_epithet:
-                message += ' | ' + record.subspecific_epithet
-            self.stdout.write(message)
+                record.species_name = species_name
+                record.subspecific_epithet = subspecific_epithet
+                record.save()
+
+                message = record.scientific_name + ' > ' + record.species_name
+                if record.subspecific_epithet:
+                    message += ' | ' + record.subspecific_epithet
+                self.stdout.write(message)
