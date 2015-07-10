a86352d02fe478d1f9303b9763900a9ec094a4fd
jnga <jnga@users.noreply.github.com>
2015-07-10 09:14:03 -0400

    Make TEMPLATE_DIRS setting a proper tuple. (see #613)

--- gobotany/settings.py
+++ gobotany/settings.py
@@ -22,7 +22,7 @@ ALLOWED_HOSTS = ['.newenglandwild.org', # any subdomain of newenglandwild.org
                 ]
 
 TEMPLATE_DIRS = (
-    os.path.join(PROJECT_ROOT, 'templates')
+    os.path.join(PROJECT_ROOT, 'templates'),
 )
 
 # We define these database specifications as constants, so that we can
