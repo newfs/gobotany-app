1b41618bacfdea3655209d6ddcae0ec7ee8108c0
jnga <jnga@users.noreply.github.com>
2016-08-03 15:17:02 -0400

    Fix issues preventing the home page from rendering. (see #655)

--- gobotany/core/partner.py
+++ gobotany/core/partner.py
@@ -2,7 +2,7 @@
 
 from django.conf import settings
 from django.http import Http404
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 from django.template import Context, RequestContext, TemplateDoesNotExist
 
 from gobotany.core.models import PartnerSite
@@ -40,8 +40,7 @@ def render_to_response_per_partner(template_name, dictionary, request,
     else:
         context_instance = Context(dictionary)
     try:
-        return render_to_response(
-            per_partner_template(request, template_name),
-            dictionary, context_instance, content_type=content_type)
+        return render(request, per_partner_template(request, template_name),
+            dictionary, content_type=content_type)
     except TemplateDoesNotExist:
         raise Http404
