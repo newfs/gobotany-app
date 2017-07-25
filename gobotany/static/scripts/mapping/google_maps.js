1617e712f162f229915dd3f08b16e5c3f8e512c4
jnga <jnga@users.noreply.github.com>
2017-07-25 14:46:58 -0400

    Add Google Maps API Key for dynamic maps called in the browser. (see #496)

--- gobotany/static/scripts/mapping/google_maps.js
+++ gobotany/static/scripts/mapping/google_maps.js
@@ -3,7 +3,7 @@
     var callback = function () {},
         callbackName = 'gmapscallback' + (new Date()).getTime();
     window[callbackName] = callback;
-    define(['//maps.googleapis.com/maps/api/js?callback=' +
+    define(['//maps.googleapis.com/maps/api/js?key=AIzaSyBKPZHo9d81hL2cP_pR3uR57AUhbn6MxkM&callback=' +
             callbackName], function () {
         return google.maps;
     });
