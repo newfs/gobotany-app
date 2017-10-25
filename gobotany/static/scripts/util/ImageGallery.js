061c9acd9170e4aa3e25baccfa5920abe02c410e
jnga <jnga@users.noreply.github.com>
2017-10-25 14:51:08 -0400

    Put species-page image gallery setup into a new module. (see #626)
    Also, adjust the height of an image gallery element.

---
+++ gobotany/static/scripts/util/ImageGallery.js
@@ -0,0 +1,98 @@
+/*
+ * Activate any class="img-container" elements to make them scrollable
+ * using their "prev" and "next" buttons.
+ * 
+ * For species pages, where the images are nested deeper in the HTML
+ * for the scrolling presentation, use a different class: scrollWrapper.
+ */
+
+define([
+    'bridge/jquery',
+    'bridge/jquery.mousewheel',
+    'bridge/shadowbox',
+    'util/PhotoHelper'
+], function ($, mousewheel, Shadowbox, PhotoHelper) {
+
+    function ImageGallery() {
+        // constructor
+    }
+    
+    ImageGallery.prototype.activate = function () {
+        // Turn on the scrollable for every gallery.
+
+        var scrollItemsSelector = '.img-container';
+        if ($('body#species').length > 0) {
+            // species page: scrolling container
+            scrollItemsSelector = '.scrollWrapper';
+        }
+        $(scrollItemsSelector).scrollable({keyboard: false});
+
+        // For each gallery, clicking its frame should bring up Shadowbox.
+
+        $('.img-gallery').each(function () {
+            var gallery = this;
+            var gallery_type = $(this).attr('data-gallery-type');
+            var photo_helper = PhotoHelper();
+
+            if (gallery_type !== undefined && gallery_type === 'link') {
+                // For a "link"-type gallery, also update a caption.
+                $(gallery).children('.img-container').on('onSeek',
+                    function(event, index) {
+                        var $items = $(this).data('scrollable').getItems();
+                        var a = $items[index];
+                        $('.img-gallery .plant-name').text(a.title);
+                        var is_scientific_name = ($(a).attr(
+                            'data-is-scientific-name') === 'true');
+                        $('.img-gallery .plant-name').toggleClass(
+                            'scientific', is_scientific_name);
+                    }
+                );
+            }
+
+            Shadowbox.setup('.img-gallery .images a', {
+                onOpen: photo_helper.prepare_to_enlarge,
+                onChange: photo_helper.prepare_to_enlarge,
+                onFinish: photo_helper.process_credit
+            });
+
+            $(gallery).click(function () {
+                var container;
+                if ($('body#species').length > 0) {
+                    // species page: deeper HTML structure created by scroller
+                    container = $(gallery).find('.scrollWrapper');
+                }
+                else {
+                    container = $(gallery).children('.img-container');
+                }
+
+                var scroll = container.data('scrollable');
+                var a = scroll.getItems()[scroll.getIndex()];
+
+                if (gallery_type !== undefined && gallery_type === 'link') {
+                    // "Link" gallery: simple hyperlink on image
+                    if (a !== undefined) {
+                        window.location.href = $(a).attr('href');
+                    }
+                }
+                else {
+                    // Default gallery type: lightboxed plant photo
+                    var rel = $(a).attr('rel');
+                    var title = $(a).attr('title');
+                    var galleryname = rel.split('[')[1].split(']')[0];
+                    Shadowbox.open({
+                        content: a,
+                        gallery: galleryname,
+                        player: 'img',
+                        title: title,
+                        options: {
+                            counterType: 'skip'
+                        }
+                    });
+                }
+            });
+        });
+    };
+
+    // Return the constructor function.
+    return ImageGallery;
+});
