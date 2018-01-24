b392484edd1609650cc66953090e6655972da24d
jnga <jnga@users.noreply.github.com>
2018-01-24 14:17:28 -0500

    Put static files settings back essentially as they were. (see #673)
    This is to try and debug why the Dev server no longer
    could find any static files (say, CSS) after the last
    deployment.

--- gobotany/settings.py
+++ gobotany/settings.py
@@ -216,12 +216,12 @@ SOUTH_MIGRATION_MODULES = {
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = ''   # os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(THIS_DIRECTORY, 'static'),   # BASE_DIR, 'static'),
 )
 
 
