fcdb682b492b4b4a240c906af7a2ae8af78947a6
jnga <jnga@users.noreply.github.com>
2018-05-10 08:43:34 -0400

    Revert to the previous reCAPTCHA library. (see #691)
    The previous library, django-recaptcha, did support
    reCAPTCHA v2, but it was hard to see in the docs.

--- gobotany/plantshare/backends/default/forms.py
+++ gobotany/plantshare/backends/default/forms.py
@@ -1,6 +1,6 @@
+from captcha.fields import ReCaptchaField
 from registration.forms import RegistrationForm
-from snowpenguin.django.recaptcha2.fields import ReCaptchaField
-from snowpenguin.django.recaptcha2.widgets import ReCaptchaWidget
+
 
 class RegistrationFormWithCaptcha(RegistrationForm):
-    captcha = ReCaptchaField(widget=ReCaptchaWidget())
+    captcha = ReCaptchaField(attrs={'theme': 'clean'})
