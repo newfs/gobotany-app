6155c8b55ca0185d6e860462207d7170de38f021
Sid Koul <skoul@newenglandwild.org>
2019-04-09 21:37:44 -0400

    Updated version number to 3.1.1.

--- gobotany/version.py
+++ gobotany/version.py
@@ -3,8 +3,4 @@ def get_version():
 	"""Simple function to return a version number. Hardcoded for the 
 	moment. """
 
-<<<<<<< HEAD
-	return "2.9.5"
-=======
-	return "3.0"
->>>>>>> release-3.0
+	return "3.1.1"
