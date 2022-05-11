a9107b636dd240e0eee26929105bae935ca132e7
jnga <jnga@users.noreply.github.com>
2022-05-10 21:42:21 -0400

    Group and subgroup pages: allow activating image gallery popup using the keyboard. (see #799)

--- gobotany/static/scripts/simplekey/simple.js
+++ gobotany/static/scripts/simplekey/simple.js
@@ -8,5 +8,20 @@ require([
             activate_video_links, glossarizer) {
     $(document).ready(function () {
         glossarizer.glossarize($('.key-char + p, .exceptions + p'));
+
+        // Allow activating the image gallery dialog using the keyboard.
+        $('.img-container').on('keydown', function (event) {
+            var keyCode = event.which;
+            if (keyCode == 32) {   // Space key
+                // Prevent page scrolling on pressing Space.
+                event.preventDefault();
+            }
+        });
+        $('.img-container').on('keyup', function (event) {
+            var keyCode = event.which;
+            if (keyCode == 13 || keyCode == 32) {   // Return, Space keys
+                $(event.target).trigger('click');
+            }
+        });
     });
 });
