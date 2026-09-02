cb985401416fed0ae39edd468bf296fa5a0e684c
jnga <jnga@users.noreply.github.com>
2026-09-01 21:41:11 -0400

    Simple/Full Keys: add keyboard support to plant preview dialog. (see #835)

--- gobotany/static/scripts/util/shadowbox_init.js
+++ gobotany/static/scripts/util/shadowbox_init.js
@@ -9,25 +9,72 @@ define([
     shadowbox_move_close_button = function () {
         var cb = document.getElementById('sb-nav-close');
         var tb = document.getElementById('sb-wrapper');
+        let interactiveElements;
+        let firstInteractiveElement;
         if (tb) {
-            tb.appendChild(cb);
+            // Get the interactive elements in the dialog.
+            interactiveElements = tb.querySelectorAll(
+                'span.gloss, button.next, button.prev, ' +
+                'a.go-to-species-page', 'a.close');
+            if (interactiveElements.length) {
+                firstInteractiveElement = interactiveElements[0];
+            }
+        }
+        if (firstInteractiveElement) {
+            firstInteractiveElement.addEventListener('keydown', function (event) {
+                if (event.shiftKey && event.key === 'Tab') {
+                    // Set focus to last element, the close button.
+                    if (cb) {
+                        cb.focus();
+
+                        // Prevent this Shift-Tab event from moving one prior.
+                        event.preventDefault();
+                    }
+                }
+            });
+        }
+        if (cb) {
+            cb.setAttribute('role', 'button');
+            cb.setAttribute('href', 'javascript:void(0);');
+            cb.addEventListener('keydown', function (event) {
+                if (event.key === ' ') {   // Space key
+                    event.preventDefault();
+                    event.target.click();
+                }
+                else if (event.key === 'Tab' && !event.shiftKey) {
+                    if (tb) {
+                        if (firstInteractiveElement) {
+                            firstInteractiveElement.focus();
+
+                            // Prevent this Tab event from moving one after.
+                            event.preventDefault();
+                        }
+                    }
+                }
+            });
+            if (tb) {
+                tb.appendChild(cb);
+                // Set initial focus on close button.
+                cb.focus();
+            }
         }
     };
 
     shadowbox_on_open = function () {
-        // Work around a bug when using lightboxes on iOS:
-        // On iOS versions older than 5, lightboxes can appear off the
-        // screen if the page is scrolled down, so scroll to the top.
-        if (navigator.userAgent.match(/(iPad|iPod|iPhone)/)) {
-            if (navigator.userAgent.match(/(OS 3_|OS 4_)/)) {
-                window.scrollTo(0, 0);
-            }
-        }
+        // Prevent the page background from scrolling.
+        document.body.style.overflow = 'hidden';
 
-        shadowbox_move_close_button();
+        // Move the close button and set up keyboard handlers after a delay
+        // to let the contents load.
+        setTimeout(function () {
+            shadowbox_move_close_button();
+        }, 1500);
     };
 
     shadowbox_on_close = function () {
+        // Allow the page background to scroll again.
+        document.body.style.overflow = 'unset';
+
         // Hide any tooltips activated from the lightbox.
         $('.gb-tooltip.dark').hide();
     };
